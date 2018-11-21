2213154c3e7f86328261d214be39cfd0ff71dc8f
aeneasr <aeneas@ory.sh>
2018-11-21 20:13:35 +0100

    dbal: Allow to omit file extension in migration
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- dbal/migrate_test.go
+++ dbal/migrate_test.go
@@ -2,6 +2,7 @@ package dbal
 
 import (
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/sirupsen/logrus"
@@ -12,18 +13,42 @@ import (
 )
 
 func TestNewPackerMigrationSource(t *testing.T) {
-	m, err := NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/b"})
+	m, err := NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/b"}, false)
 	require.NoError(t, err)
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/1.sql"), "%v", m.Box.List())
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/2.sql"), "%v", m.Box.List())
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/3.sql"), "%v", m.Box.List())
 
-	m, err = NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/c"})
+	m, err = NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/c"}, false)
 	require.NoError(t, err)
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/1.sql"), "%v", m.Box.List())
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/2.sql"), "%v", m.Box.List())
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/3.sql"), "%v", m.Box.List())
 	assert.True(t, stringslice.Has(m.Box.List(), "/migrations/sql/4.sql"), "%v", m.Box.List())
+}
+
+func TestPackerOmitExtensionEnabled(t *testing.T) {
+	m, err := NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/b"}, true)
+	require.NoError(t, err)
+
+	ms, err := m.FindMigrations()
+	require.NoError(t, err)
+
+	for _, mm := range ms {
+		require.False(t, strings.Contains(mm.Id, ".sql"))
+	}
+}
+
+func TestPackerOmitExtensionDisabled(t *testing.T) {
+	m, err := NewPackerMigrationSource(logrus.New(), AssetNames(), Asset, []string{"stub/a", "stub/b"}, false)
+	require.NoError(t, err)
+
+	ms, err := m.FindMigrations()
+	require.NoError(t, err)
+
+	for _, mm := range ms {
+		require.True(t, strings.Contains(mm.Id, ".sql"))
+	}
 }
 
 func TestMigrationFileSort(t *testing.T) {
