a8d2754441855f503da110a80034749abd0bc64d
aeneasr <aeneas@ory.sh>
2019-03-07 19:16:48 +0100

    stringsx: Add lower/uppercase-ing methods for initials
    Signed-off-by: aeneasr <aeneas@ory.sh>

---
+++ stringsx/case_test.go
@@ -0,0 +1,23 @@
+package stringsx
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestToLowerInitial(t *testing.T) {
+	assert.Equal(t, "", ToLowerInitial(""))
+	assert.Equal(t, "a", ToLowerInitial("a"))
+	assert.Equal(t, "a", ToLowerInitial("A"))
+	assert.Equal(t, "ab", ToLowerInitial("Ab"))
+	assert.Equal(t, "aA", ToLowerInitial("AA"))
+}
+
+
+func TestToUpperInitial(t *testing.T) {
+	assert.Equal(t, "", ToUpperInitial(""))
+	assert.Equal(t, "A", ToUpperInitial("a"))
+	assert.Equal(t, "A", ToUpperInitial("A"))
+	assert.Equal(t, "AB", ToUpperInitial("aB"))
+	assert.Equal(t, "Ab", ToUpperInitial("ab"))
+}
