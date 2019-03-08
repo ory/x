b9d1b009c1e90d26c72c27b9a79c1d82137b06b9
aeneasr <aeneas@ory.sh>
2019-03-08 09:26:18 +0100

    Resolve linter issues
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- urlx/parse.go
+++ urlx/parse.go
@@ -1,42 +1,43 @@
 package urlx
 
 import (
-	"github.com/sirupsen/logrus"
 	"net/url"
+
+	"github.com/sirupsen/logrus"
 )
 
 // ParseOrPanic parses a url or panics.
 func ParseOrPanic(in string) *url.URL {
 	out, err := url.Parse(in)
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
 	return out
 }
 
-// ParseOrPanic parses a url or fatals.
+// ParseOrFatal parses a url or fatals.
 func ParseOrFatal(l logrus.FieldLogger, in string) *url.URL {
 	out, err := url.Parse(in)
 	if err != nil {
-		l.WithError(err).Fatal("Unable to parse url: %s", in)
+		l.WithError(err).Fatalf("Unable to parse url: %s", in)
 	}
 	return out
 }
 
-// ParseOrPanic parses a request uri or panics.
+// ParseRequestURIOrPanic parses a request uri or panics.
 func ParseRequestURIOrPanic(in string) *url.URL {
 	out, err := url.ParseRequestURI(in)
 	if err != nil {
-		panic(err)
+		panic(err.Error())
 	}
 	return out
 }
 
-// ParseOrPanic parses a request uri or fatals.
+// ParseRequestURIOrFatal parses a request uri or fatals.
 func ParseRequestURIOrFatal(l logrus.FieldLogger, in string) *url.URL {
 	out, err := url.ParseRequestURI(in)
 	if err != nil {
-		l.WithError(err).Fatal("Unable to parse url: %s", in)
+		l.WithError(err).Fatalf("Unable to parse url: %s", in)
 	}
 	return out
 }
