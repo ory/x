67b11e2de8304c7e45c9d8c769e2b1f0ec76de53
aeneasr <aeneas@ory.sh>
2019-04-12 15:30:05 +0200

    pointerx: Add reverse and bool helper
    Signed-off-by: aeneasr <aeneas@ory.sh>

--- pointerx/pointerx.go
+++ pointerx/pointerx.go
@@ -5,9 +5,25 @@ func String(s string) *string {
 	return &s
 }
 
+// StringR is the reverse to String.
+func StringR(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return *s
+}
+
 // Int returns the input value's pointer.
 func Int(s int) *int {
 	return &s
+}
+
+// IntR is the reverse to Int.
+func IntR(s *int) int {
+	if s == nil {
+		return int(0)
+	}
+	return *s
 }
 
 // Int32 returns the input value's pointer.
@@ -15,9 +31,25 @@ func Int32(s int32) *int32 {
 	return &s
 }
 
+// Int32R is the reverse to Int32.
+func Int32R(s *int32) int32 {
+	if s == nil {
+		return int32(0)
+	}
+	return *s
+}
+
 // Int64 returns the input value's pointer.
 func Int64(s int64) *int64 {
 	return &s
+}
+
+// Int64R is the reverse to Int64.
+func Int64R(s *int64) int64 {
+	if s == nil {
+		return int64(0)
+	}
+	return *s
 }
 
 // Float32 returns the input value's pointer.
@@ -25,7 +57,36 @@ func Float32(s float32) *float32 {
 	return &s
 }
 
+// Float32R is the reverse to Float32.
+func Float32R(s *float32) float32 {
+	if s == nil {
+		return float32(0)
+	}
+	return *s
+}
+
 // Float64 returns the input value's pointer.
 func Float64(s float64) *float64 {
 	return &s
+}
+
+// Float64R is the reverse to Float64.
+func Float64R(s *float64) float64 {
+	if s == nil {
+		return float64(0)
+	}
+	return *s
+}
+
+// Bool returns the input value's pointer.
+func Bool(s bool) *bool {
+	return &s
+}
+
+// BoolR is the reverse to Bool.
+func BoolR(s *bool) bool {
+	if s == nil {
+		return false
+	}
+	return *s
 }
