826c1e6beb3c86c0002b5f815eed3a66f3394b27
Kevin Minehart <kmineh0151@gmail.com>
2019-04-16 12:35:34 -0500

    Use http.ResponseWriter instead of returning strings
    Signed-off-by: Kevin Minehart <kmineh0151@gmail.com>

--- pagination/header.go
+++ pagination/header.go
@@ -2,6 +2,7 @@ package pagination
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
@@ -15,11 +16,11 @@ func header(u *url.URL, rel string, limit, offset int) string {
 	return fmt.Sprintf("<%s>; rel=\"%s\"", u.String(), rel)
 }
 
-// Header returns string to be used in an http header map for pagination where backwards compatibility is required.
-// The value returned will contain links any combination of the first, last, next, or previous (prev) pages in a paginated list (given a limit and an offset, and optionally a total).
+// Header adds an http header for pagination using a responsewriter where backwards compatibility is required.
+// The header will contain links any combination of the first, last, next, or previous (prev) pages in a paginated list (given a limit and an offset, and optionally a total).
 // If total is not set, then no "last" page will be calculated.
 // If no limit is provided, then it will default to 1.
-func Header(u *url.URL, total int, limit, offset int) string {
+func Header(w http.ResponseWriter, u *url.URL, total int, limit, offset int) {
 	if limit <= 0 {
 		limit = 1
 	}
@@ -36,30 +37,33 @@ func Header(u *url.URL, total int, limit, offset int) string {
 	// Check for last page
 	if offset >= lastOffset {
 		if total == 0 {
-			return strings.Join([]string{
+			w.Header().Set("Link", strings.Join([]string{
 				header(u, "first", limit, 0),
 				header(u, "next", limit, ((offset/limit)+1)*limit),
 				header(u, "prev", limit, ((offset/limit)-1)*limit),
-			}, ",")
+			}, ","))
+			return
 		}
-		return strings.Join([]string{
+		w.Header().Set("Link", strings.Join([]string{
 			header(u, "first", limit, 0),
 			header(u, "prev", limit, lastOffset-limit),
-		}, ",")
+		}, ","))
+		return
 	}
 
 	if offset < limit {
-		return strings.Join([]string{
+		w.Header().Set("Link", strings.Join([]string{
 			header(u, "next", limit, limit),
 			header(u, "last", limit, lastOffset),
-		}, ",")
+		}, ","))
+		return
 	}
 
-	return strings.Join([]string{
+	w.Header().Set("Link", strings.Join([]string{
 		header(u, "first", limit, 0),
 		header(u, "next", limit, ((offset/limit)+1)*limit),
 		header(u, "prev", limit, ((offset/limit)-1)*limit),
 		header(u, "last", limit, lastOffset),
-	}, ",")
-
+	}, ","))
+	return
 }
