1faa2d657d2827a9707102b5f2ccd6fe0c293bda
aeneasr <aeneas@ory.sh>
2019-07-13 12:40:13 +0200

    jsonx: Add NewStrictDecoder

--- jsonx/decoder.go
+++ jsonx/decoder.go
@@ -5,6 +5,7 @@ import (
 	"io"
 )
 
+// NewStrictDecoder is a shorthand for json.Decoder.DisallowUnknownFields
 func NewStrictDecoder(b io.Reader) *json.Decoder {
 	d := json.NewDecoder(b)
 	d.DisallowUnknownFields()
