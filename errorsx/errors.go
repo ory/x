ae9a78bdcd48f77a65aab998de1c5a3c8f37a505
aeneasr <aeneas@ory.sh>
2019-11-26 12:28:18 +0100

    errorsx: Add package for dealing with nil-causers.

---
+++ errorsx/errors.go
@@ -0,0 +1,27 @@
+package errorsx
+
+// Cause returns the underlying cause of the error, if possible.
+// An error value has a cause if it implements the following
+// interface:
+//
+//     type causer interface {
+//            Cause() error
+//     }
+//
+// If the error does not implement Cause, the original error will
+// be returned. If the error is nil, nil will be returned without further
+// investigation.
+func Cause(err error) error {
+	type causer interface {
+		Cause() error
+	}
+
+	for err != nil {
+		cause, ok := err.(causer)
+		if !ok || cause.Cause() == nil {
+			break
+		}
+		err = cause.Cause()
+	}
+	return err
+}
