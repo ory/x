1b3b54bc96060296f74faa5f89ec6a66261e85fd
aeneasr <aeneas@ory.sh>
2020-04-03 12:12:16 +0200

    feat(httpx): decompressable http requests

---
+++ httpx/gzip_server.go
@@ -0,0 +1,58 @@
+package httpx
+
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type CompressionRequestReader struct {
+	ErrHandler func(w http.ResponseWriter, r *http.Request, err error)
+}
+
+func defaultCompressionErrorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
+}
+
+func NewCompressionRequestReader(eh func(w http.ResponseWriter, r *http.Request, err error)) *CompressionRequestReader {
+	if eh == nil {
+		eh = defaultCompressionErrorHandler
+	}
+
+	return &CompressionRequestReader{
+		ErrHandler: eh,
+	}
+}
+
+func (c *CompressionRequestReader) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	for _, enc := range strings.Split(r.Header.Get("Content-Encoding"), ",") {
+		switch enc = strings.TrimSpace(enc); enc {
+		case "gzip":
+			var b bytes.Buffer
+			reader, err := gzip.NewReader(r.Body)
+			if err != nil {
+				c.ErrHandler(w, r, err)
+				return
+			}
+
+			if _, err := io.Copy(&b, reader); err != nil {
+				c.ErrHandler(w, r, err)
+				return
+			}
+
+			r.Body = ioutil.NopCloser(&b)
+		case "identity":
+			fallthrough
+		case "":
+			// nothing to do
+		default:
+			c.ErrHandler(w, r, fmt.Errorf("%s content encoding not supported", enc))
+		}
+	}
+
+	next(w, r)
+}
