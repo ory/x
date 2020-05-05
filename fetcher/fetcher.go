18f05008d06dbea8f6157755e087a2a82ae414b3
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-05-05 14:09:27 +0200

    feat: add fetcher module (#144)

---
+++ fetcher/fetcher.go
@@ -0,0 +1,94 @@
+package fetcher
+
+import (
+	"bytes"
+	"encoding/base64"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+
+	"github.com/ory/x/httpx"
+)
+
+// Fetcher is able to load file contents from http, https, file, and base64 locations.
+type Fetcher struct {
+	hc *http.Client
+}
+
+type opts struct {
+	hc *http.Client
+}
+
+// WithClient sets the http.Client the fetcher uses.
+func WithClient(hc *http.Client) func(*opts) {
+	return func(o *opts) {
+		o.hc = hc
+	}
+}
+
+func newOpts() *opts {
+	return &opts{
+		hc: httpx.NewResilientClientLatencyToleranceMedium(nil),
+	}
+}
+
+// NewFetcher creates a new fetcher instance.
+func NewFetcher(opts ...func(*opts)) *Fetcher {
+	o := newOpts()
+	for _, f := range opts {
+		f(o)
+	}
+	return &Fetcher{hc: o.hc}
+}
+
+// Fetch fetches the file contents from the source.
+func (f *Fetcher) Fetch(source string) (*bytes.Buffer, error) {
+	if strings.HasPrefix(source, "http") || strings.HasPrefix(source, "https") {
+		return f.fetchRemote(source)
+	} else if strings.HasPrefix(source, "file") {
+		return f.fetchFile(strings.Replace(source, "file://", "", 1))
+	} else if strings.HasPrefix(source, "base64") {
+		src, err := base64.StdEncoding.DecodeString(strings.Replace(source, "base64://", "", 1))
+		if err != nil {
+			return nil, errors.Wrapf(err, "rule: %s", source)
+		}
+		return bytes.NewBuffer(src), nil
+	}
+
+	return nil, errors.Errorf("source url uses an unknown scheme: %s", source)
+}
+
+func (f *Fetcher) fetchRemote(source string) (*bytes.Buffer, error) {
+	res, err := f.hc.Get(source)
+	if err != nil {
+		return nil, errors.Wrapf(err, "rule: %s", source)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("expected http response status code 200 but got %d when fetching: %s", res.StatusCode, source)
+	}
+
+	return f.decode(res.Body)
+}
+
+func (f *Fetcher) fetchFile(source string) (*bytes.Buffer, error) {
+	fp, err := os.Open(source)
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to fetch from source: %s", source)
+	}
+	defer fp.Close()
+
+	return f.decode(fp)
+}
+
+func (f *Fetcher) decode(r io.Reader) (*bytes.Buffer, error) {
+	var b bytes.Buffer
+	if _, err := io.Copy(&b, r); err != nil {
+		return nil, err
+	}
+	return &b, nil
+}
---
+++ fetcher/fetcher_test.go
@@ -0,0 +1,60 @@
+package fetcher
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/gobuffalo/httptest"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFetcher(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		_, _ = w.Write([]byte(`{"foo":"bar"}`))
+	})
+	ts := httptest.NewServer(router)
+	t.Cleanup(ts.Close)
+
+	file, err := ioutil.TempFile(os.TempDir(), "source.*.json")
+	require.NoError(t, err)
+
+	_, err = file.WriteString(`{"foo":"baz"}`)
+	require.NoError(t, err)
+	require.NoError(t, file.Close())
+
+	for fc, fetcher := range []*Fetcher{
+		NewFetcher(WithClient(ts.Client())),
+		NewFetcher(),
+	} {
+		for k, tc := range []struct {
+			source string
+			expect string
+		}{
+			{
+				source: "base64://" + base64.StdEncoding.EncodeToString([]byte(`{"foo":"zab"}`)),
+				expect: `{"foo":"zab"}`,
+			},
+			{
+				source: "file://" + file.Name(),
+				expect: `{"foo":"baz"}`,
+			},
+			{
+				source: ts.URL,
+				expect: `{"foo":"bar"}`,
+			},
+		} {
+			t.Run(fmt.Sprintf("config=%d/case=%d", fc, k), func(t *testing.T) {
+				actual, err := fetcher.Fetch(tc.source)
+				require.NoError(t, err)
+				assert.JSONEq(t, tc.expect, actual.String())
+			})
+		}
+	}
+}
