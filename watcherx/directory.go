324940adbe20bf6116acd427b66572eb5f450525
zepatrik <patrik@ory.sh>
2020-08-03 19:07:40 +0200

    feat: watchx add directory watcher

--- watcherx/directory.go
+++ watcherx/directory.go
@@ -4,92 +4,117 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/fsnotify/fsnotify"
-	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
-	"path/filepath"
+	"path"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/pkg/errors"
 )
 
-func listSubDirsDepth(dirs []string, errs []error, parent string, depth int) ([]string, []error) {
+func listSubDirsDepth(parent string, depth int) (dirs []string, errs []error) {
 	if depth == 0 {
-		return dirs, nil
+		return
 	}
-	files, err := ioutil.ReadDir(parent)
-
+	entries, err := ioutil.ReadDir(parent)
+	if err != nil {
+		return dirs, append(errs, errors.WithStack(err))
+	}
+	for _, e := range entries {
+		if e.IsDir() {
+			dn := path.Join(parent, e.Name())
+			subDirs, subErrs := listSubDirsDepth(dn, depth-1)
+			dirs = append(append(dirs, dn), subDirs...)
+			errs = append(errs, subErrs...)
+		}
+	}
+	return
 }
 
-func WatchDirectory(ctx context.Context, dir string, c EventChannel) error {
+func WatchDirectory(ctx context.Context, dir string, depth int, c EventChannel) error {
 	w, err := fsnotify.NewWatcher()
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	if err := w.Add(dir); err != nil {
-		return errors.WithStack(err)
+	subDirs, errs := listSubDirsDepth(dir, depth)
+	if len(errs) != 0 {
+		return errors.Errorf("%+v", errs)
 	}
-	ioutil.ReadDir()
-	if err := filepath.Walk(dir, func(p string, info os.FileInfo, err error) error {
-		if err != nil {
+	for _, d := range append(subDirs, dir) {
+		fmt.Printf("adding watcher for %s\n", d)
+		if err := w.Add(d); err != nil {
 			return errors.WithStack(err)
 		}
-		// there can't be an error as we know p is within dir
-		rel, _ := filepath.Rel(dir, p)
-		if len()
-		if info.IsDir() {
-			if err := w.Add(p); err != nil {
-				return errors.WithStack(err)
-			}
-		}
-		return nil
-	}); err != nil {
-		return err
 	}
-	go streamDirectoryEvents(ctx, w, dir, c)
+	go streamDirectoryEvents(ctx, w, c)
 	return nil
 }
 
-func streamDirectoryEvents(ctx context.Context, w *fsnotify.Watcher, dir string, c EventChannel) {
+func handleEvent(e fsnotify.Event, w *fsnotify.Watcher, c EventChannel) {
+	if e.Op&fsnotify.Remove != 0 {
+		// We cannot figure out anymore if it was a file or directory.
+		// If it was a directory it was added to the watchers as well as it's parent.
+		// Therefore we will get two consecutive remove events from inotify (REMOVE and REMOVE_SELF).
+		// Sometimes the second event has an empty name (no specific reason for that).
+		// If there is no second event (timeout 1ms) we assume it was a file that got deleted.
+		// This means that file deletion events are delayed by 1ms.
+		select {
+		case <-time.After(time.Millisecond):
+			c <- &RemoveEvent{
+				source: source(e.Name),
+			}
+			return
+		case secondE := <-w.Events:
+			if (secondE.Name != "" && secondE.Name != e.Name) || secondE.Op&fsnotify.Remove == 0 {
+				// this is NOT the unix.IN_DELETE_SELF event => we have to handle the first explicitly
+				// and the second recursively because it might be the first event of a directory deletion
+				c <- &RemoveEvent{
+					source: source(e.Name),
+				}
+				handleEvent(secondE, w, c)
+			} // else we do not want any event on deletion of a folder
+		}
+	} else if e.Op&(fsnotify.Write|fsnotify.Create) != 0 {
+		if stats, err := os.Stat(e.Name); err != nil {
+			c <- &ErrorEvent{
+				error:  errors.WithStack(err),
+				source: source(e.Name),
+			}
+			return
+		} else if stats.IsDir() {
+			fmt.Printf("created directory %s\n", e.Name)
+			if err := w.Add(e.Name); err != nil {
+				c <- &ErrorEvent{
+					error:  errors.WithStack(err),
+					source: source(e.Name),
+				}
+			}
+			return
+		}
+		data, err := ioutil.ReadFile(e.Name)
+		if err != nil {
+			c <- &ErrorEvent{
+				error:  err,
+				source: source(e.Name),
+			}
+		} else {
+			c <- &ChangeEvent{
+				data:   bytes.NewBuffer(data),
+				source: source(e.Name),
+			}
+		}
+	}
+}
+
+func streamDirectoryEvents(ctx context.Context, w *fsnotify.Watcher, c EventChannel) {
 	for {
 		select {
 		case <-ctx.Done():
 			_ = w.Close()
 			return
 		case e := <-w.Events:
-			fmt.Printf("got event %s\n", e.String())
-			if e.Op&(fsnotify.Write|fsnotify.Create) != 0 {
-				if stats, err := os.Stat(e.Name); err != nil {
-					c <- &ErrorEvent{
-						error:  errors.WithStack(err),
-						source: source(e.Name),
-					}
-					continue
-				} else if stats.IsDir() {
-					fmt.Printf("created directory %s\n", e.Name)
-					if err := w.Add(e.Name); err != nil {
-						c <- &ErrorEvent{
-							error:  errors.WithStack(err),
-							source: source(e.Name),
-						}
-					}
-					continue
-				}
-				data, err := ioutil.ReadFile(e.Name)
-				if err != nil {
-					c <- &ErrorEvent{
-						error:  err,
-						source: source(e.Name),
-					}
-				} else {
-					c <- &ChangeEvent{
-						data:   bytes.NewBuffer(data),
-						source: source(e.Name),
-					}
-				}
-			} else if e.Op&fsnotify.Remove != 0 {
-				c <- &RemoveEvent{
-					source: source(e.Name),
-				}
-			}
+			handleEvent(e, w, c)
 		}
 	}
 }
--- watcherx/directory_test.go
+++ watcherx/directory_test.go
@@ -2,19 +2,96 @@ package watcherx
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ory/x/stringslice"
 )
+
+func TestListSubDirsDepth(t *testing.T) {
+	absSetup := func(t *testing.T) (string, []string) {
+		wd, err := ioutil.TempDir("", "list-sub-dir-test-")
+		var dirs []string
+		for _, d := range []string{
+			"bar",
+			"bar/baz",
+			"foo",
+		} {
+			dirs = append(dirs, path.Join(wd, d))
+		}
+		require.NoError(t, err)
+
+		for _, d := range dirs {
+			require.NoError(t, os.MkdirAll(d, 0777))
+		}
+
+		return wd, dirs
+	}
+
+	relSetup := func(t *testing.T) (func(), []string) {
+		dirs := []string{
+			"bar",
+			"bar/baz",
+			"foo",
+		}
+		wd, err := ioutil.TempDir("", "list-sub-dir-test-")
+		require.NoError(t, err)
+
+		for _, d := range dirs {
+			require.NoError(t, os.MkdirAll(path.Join(wd, d), 0777))
+		}
+
+		prevWD, err := os.Getwd()
+		require.NoError(t, err)
+		require.NoError(t, os.Chdir(wd))
+		return func() {
+			require.NoError(t, os.Chdir(prevWD))
+		}, dirs
+	}
+
+	t.Run("case=absolute path", func(t *testing.T) {
+		wd, dirs := absSetup(t)
+
+		subDirs, errs := listSubDirsDepth(wd, 1000)
+		require.Equal(t, 0, len(errs), "%+v", errs)
+		assert.Equal(t, dirs, subDirs)
+	})
+
+	t.Run("case=relative path", func(t *testing.T) {
+		clean, dirs := relSetup(t)
+		defer clean()
+
+		subDirs, errs := listSubDirsDepth(".", 100)
+
+		require.Equal(t, 0, len(errs), "%+v", errs)
+		assert.Equal(t, dirs, subDirs)
+	})
+
+	t.Run("case=respects depth", func(t *testing.T) {
+		clean, dirs := relSetup(t)
+		defer clean()
+
+		subDirs, errs := listSubDirsDepth(".", 1)
+		require.Equal(t, 0, len(errs), "%+v", errs)
+		assert.Equal(t, stringslice.Filter(dirs, func(s string) bool {
+			return len(strings.Split(s, string(os.PathSeparator))) > 1
+		}), subDirs)
+	})
+}
 
 func TestWatchDirectory(t *testing.T) {
 	t.Run("case=notifies about file creation in directory", func(t *testing.T) {
 		ctx, c, dir, cancel := setup(t)
 		defer cancel()
 
-		require.NoError(t, WatchDirectory(ctx, dir, c))
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
 		fileName := path.Join(dir, "example")
 		f, err := os.Create(fileName)
 		require.NoError(t, err)
@@ -30,7 +107,7 @@ func TestWatchDirectory(t *testing.T) {
 		fileName := path.Join(dir, "example")
 		f, err := os.Create(fileName)
 		require.NoError(t, err)
-		require.NoError(t, WatchDirectory(ctx, dir, c))
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
 
 		_, err = fmt.Fprintf(f, "content")
 		require.NoError(t, f.Close())
@@ -47,7 +124,7 @@ func TestWatchDirectory(t *testing.T) {
 		require.NoError(t, err)
 		require.NoError(t, f.Close())
 
-		require.NoError(t, WatchDirectory(ctx, dir, c))
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
 		require.NoError(t, os.Remove(fileName))
 
 		assertRemove(t, <-c, fileName)
@@ -60,7 +137,7 @@ func TestWatchDirectory(t *testing.T) {
 		childDir := path.Join(dir, "child")
 		require.NoError(t, os.Mkdir(childDir, 0777))
 
-		require.NoError(t, WatchDirectory(ctx, dir, c))
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
 
 		fileName := path.Join(childDir, "example")
 		f, err := os.Create(fileName)
@@ -74,7 +151,7 @@ func TestWatchDirectory(t *testing.T) {
 		ctx, c, dir, cancel := setup(t)
 		defer cancel()
 
-		require.NoError(t, WatchDirectory(ctx, dir, c))
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
 
 		childDir := path.Join(dir, "child")
 		require.NoError(t, os.Mkdir(childDir, 0777))
@@ -86,5 +163,49 @@ func TestWatchDirectory(t *testing.T) {
 		require.NoError(t, f.Close())
 
 		assertChange(t, <-c, "", fileName)
+	})
+
+	t.Run("case=does not notify on directory deletion", func(t *testing.T) {
+		ctx, c, dir, cancel := setup(t)
+		defer cancel()
+
+		childDir := path.Join(dir, "child")
+		require.NoError(t, os.Mkdir(childDir, 0777))
+
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
+
+		require.NoError(t, os.Remove(childDir))
+
+		select {
+		case e := <-c:
+			t.Logf("got unexpected event %T: %+v", e, e)
+			t.FailNow()
+		case <-time.After(2 * time.Millisecond):
+			// expected to not receive an event (1ms is what the watcher waits for the second event)
+		}
+	})
+
+	t.Run("case=notifies only for files on batch delete", func(t *testing.T) {
+		ctx, c, dir, cancel := setup(t)
+		defer cancel()
+
+		childDir := path.Join(dir, "child")
+		subChildDir := path.Join(childDir, "subchild")
+		require.NoError(t, os.MkdirAll(subChildDir, 0777))
+		f1 := path.Join(subChildDir, "f1")
+		f, err := os.Create(f1)
+		require.NoError(t, err)
+		require.NoError(t, f.Close())
+		f2 := path.Join(childDir, "f2")
+		f, err = os.Create(f2)
+		require.NoError(t, err)
+		require.NoError(t, f.Close())
+
+		require.NoError(t, WatchDirectory(ctx, dir, 100, c))
+
+		require.NoError(t, os.RemoveAll(childDir))
+
+		assertRemove(t, <-c, f2)
+		assertRemove(t, <-c, f1)
 	})
 }
