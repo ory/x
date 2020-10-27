037470c1d7284864f09b34052e7169b80156b1a7
Patrik Lindahl <patrik@ramnet.se>
2020-10-27 19:12:20 +0800

    fix: functions to parse URLs and handle URL file paths (#219)
    This code was first created for PR #557 in oathkeeper.
    It should probably be in the shared ory/x project instead of there,
    so that is why this code is checked in here.
    
    Co-authored-by: zepatrik <patrik@ory.sh>
    Co-authored-by: Patrik <zepatrik@users.noreply.github.com>
    Co-authored-by: hackerman <3372410+aeneasr@users.noreply.github.com>

--- urlx/parse.go
+++ urlx/parse.go
@@ -2,13 +2,54 @@ package urlx
 
 import (
 	"net/url"
+	"regexp"
+	"strings"
 
 	"github.com/ory/x/logrusx"
 )
 
+// winPathRegex is a regex for [DRIVE-LETTER]:
+var winPathRegex = regexp.MustCompile("^[A-Za-z]:.*")
+
+// Parse parses rawURL into a URL structure with special handling for file:// URLs
+// File URLs with relative paths (file://../file, ../file) will be returned as a
+// url.URL object without the Scheme set to "file". This is because the file
+// scheme doesn't support relative paths. Make sure to check for
+// both "file" or "" (an empty string) in URL.Scheme if you are looking for
+// a file path.
+// Use the companion function GetURLFilePath() to get a file path suitable
+// for the current operaring system.
+func Parse(rawURL string) (*url.URL, error) {
+	lcRawURL := strings.ToLower(rawURL)
+	if strings.HasPrefix(lcRawURL, "file:///") {
+		return url.Parse(rawURL)
+	}
+
+	// Normally the first part after file:// is a hostname, but since
+	// this is often misused we interpret the URL like a normal path
+	// by removing the "file://" from the beginning (if it exists)
+	rawURL = trimPrefixIC(rawURL, "file://")
+
+	if winPathRegex.MatchString(rawURL) {
+		// Windows path
+		return url.Parse("file:///" + rawURL)
+	}
+
+	if strings.HasPrefix(lcRawURL, "\\\\") {
+		// Windows UNC path
+		// We extract the hostname and create an appropriate file:// URL
+		// based on the hostname and the path
+		host, path := extractUNCPathParts(rawURL)
+		// It is safe to replace the \ with / here because this is POSIX style path
+		return url.Parse("file://" + host + strings.ReplaceAll(path, "\\", "/"))
+	}
+
+	return url.Parse(rawURL)
+}
+
 // ParseOrPanic parses a url or panics.
 func ParseOrPanic(in string) *url.URL {
-	out, err := url.Parse(in)
+	out, err := Parse(in)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -17,7 +58,7 @@ func ParseOrPanic(in string) *url.URL {
 
 // ParseOrFatal parses a url or fatals.
 func ParseOrFatal(l *logrusx.Logger, in string) *url.URL {
-	out, err := url.Parse(in)
+	out, err := Parse(in)
 	if err != nil {
 		l.WithError(err).Fatalf("Unable to parse url: %s", in)
 	}
@@ -40,4 +81,23 @@ func ParseRequestURIOrFatal(l *logrusx.Logger, in string) *url.URL {
 		l.WithError(err).Fatalf("Unable to parse url: %s", in)
 	}
 	return out
+}
+
+func extractUNCPathParts(uncPath string) (host, path string) {
+	parts := strings.Split(strings.TrimPrefix(uncPath, "\\\\"), "\\")
+	host = parts[0]
+	if len(parts) > 0 {
+		path = "\\" + strings.Join(parts[1:], "\\")
+	}
+	return host, path
+}
+
+// trimPrefixIC returns s without the provided leading prefix string using
+// case insensitive matching.
+// If s doesn't start with prefix, s is returned unchanged.
+func trimPrefixIC(s, prefix string) string {
+	if strings.HasPrefix(strings.ToLower(s), prefix) {
+		return s[len(prefix):]
+	}
+	return s
 }
---
+++ urlx/path.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package urlx
+
+import (
+	"net/url"
+)
+
+// GetURLFilePath returns the path of a URL that is compatible with the runtime os filesystem
+func GetURLFilePath(u *url.URL) string {
+	if u == nil {
+		return ""
+	}
+	return u.Path
+}
