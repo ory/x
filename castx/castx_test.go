a3870d48dc0fac908aebe169fc6987785144d85a
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-20 16:36:36 +0100

    feat(castx): add float casters

---
+++ castx/castx_test.go
@@ -0,0 +1,44 @@
+package castx
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToFloatSliceE(t *testing.T) {
+	tests := []struct {
+		input  interface{}
+		expect []float64
+		iserr  bool
+	}{
+		{[]int{1, 3}, []float64{1, 3}, false},
+		{[]interface{}{1.2, 3.2}, []float64{1.2, 3.2}, false},
+		{[]string{"2", "3"}, []float64{2, 3}, false},
+		{[]string{"2.2", "3.2"}, []float64{2.2, 3.2}, false},
+		{[2]string{"2", "3"}, []float64{2, 3}, false},
+		{[2]string{"2.2", "3.2"}, []float64{2.2, 3.2}, false},
+		// errors
+		{nil, nil, true},
+		{testing.T{}, nil, true},
+		{[]string{"foo", "bar"}, nil, true},
+	}
+
+	for i, test := range tests {
+		errmsg := fmt.Sprintf("i = %d", i) // assert helper message
+
+		v, err := ToFloatSliceE(test.input)
+		if test.iserr {
+			assert.Error(t, err, errmsg)
+			continue
+		}
+
+		assert.NoError(t, err, errmsg)
+		assert.Equal(t, test.expect, v, errmsg)
+
+		// Non-E test
+		v = ToFloatSlice(test.input)
+		assert.Equal(t, test.expect, v, errmsg)
+	}
+}
