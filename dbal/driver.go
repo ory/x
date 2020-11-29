6595834c48a544036ed002e22f88781f694434fc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-29 18:42:07 +0100

    feat(dbal): improve sqlite support and missing error
    Closes https://github.com/ory/hydra/issues/2164

--- dbal/driver.go
+++ dbal/driver.go
@@ -1,6 +1,7 @@
 package dbal
 
 import (
+	"strings"
 	"sync"
 
 	"github.com/pkg/errors"
@@ -11,7 +12,12 @@ var (
 	dmtx    sync.Mutex
 
 	// ErrNoResponsibleDriverFound is returned when no driver was found for the provided DSN.
-	ErrNoResponsibleDriverFound = errors.New("no driver is capable of handling the given DSN")
+	ErrNoResponsibleDriverFound = errors.New("dsn value requested an unknown driver")
+	ErrSQLiteSupportMissing     = errors.New(`the DSN connection string looks like a SQLite connection, but SQLite support was not built into the binary. Please check if you have downloaded the correct binary or are using the correct Docker Image. Binary archives and Docker Images indicate SQLite support by appending the -sqlite suffix.`)
+)
+
+const (
+	InMemoryDSN = "sqlite://:memory:?_fk=true"
 )
 
 // Driver represents a driver
@@ -38,5 +44,16 @@ func GetDriverFor(dsn string) (Driver, error) {
 			return driver, nil
 		}
 	}
+
+	if IsSQLite(dsn) {
+		return nil, ErrSQLiteSupportMissing
+	}
+
 	return nil, ErrNoResponsibleDriverFound
+}
+
+// IsSQLite returns true if the connection is a SQLite string.
+func IsSQLite(dsn string) bool {
+	scheme := strings.Split(dsn, "://")[0]
+	return scheme == "sqlite" || scheme == "sqlite3"
 }
