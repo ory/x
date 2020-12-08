367bd045fe611670197865727351370232d0eea8
Patrik <zepatrik@users.noreply.github.com>
2020-12-08 12:34:09 +0100

    feat: add ErrUnknownCase to stringsx.RegisteredCases (#242)

--- stringsx/switch_case.go
+++ stringsx/switch_case.go
@@ -1,8 +1,42 @@
 package stringsx
 
-type RegisteredCases []string
+import (
+	"fmt"
+	"strings"
+)
+
+type (
+	RegisteredCases []string
+	errUnknownCase  struct {
+		cases  RegisteredCases
+		actual string
+	}
+)
+
+var ErrUnknownCase = errUnknownCase{}
 
 func (r *RegisteredCases) AddCase(c string) string {
 	*r = append(*r, c)
 	return c
+}
+
+func (r *RegisteredCases) String() string {
+	s := make([]string, len(*r))
+	for i, c := range *r {
+		s[i] = fmt.Sprintf("%#v", c)
+	}
+	return "[" + strings.Join(s, ", ") + "]"
+}
+
+func (r *RegisteredCases) ToUnknownCaseErr(actual string) error {
+	return errUnknownCase{cases: *r, actual: actual}
+}
+
+func (e errUnknownCase) Error() string {
+	return fmt.Sprintf("expected one of %s but got %s", e.cases.String(), e.actual)
+}
+
+func (e errUnknownCase) Is(err error) bool {
+	_, ok := err.(errUnknownCase)
+	return ok
 }
