9c1065b74d6b5808ad4470a5b50a2527dc6048fd
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-12-14 15:42:41 +0100

    feat(jsonx): add test helpers

---
+++ jsonx/helpers.go
@@ -0,0 +1,18 @@
+package jsonx
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMarshalJSONString(t *testing.T, i interface{}) string {
+	out, err := json.Marshal(i)
+	require.NoError(t, err)
+	return string(out)
+}
+
+func TestUnmarshalJSON(t *testing.T, in []byte, i interface{}) {
+	require.NoError(t, json.Unmarshal(in, i))
+}
