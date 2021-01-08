32720068623594b0a03f3e1d4d7624a9ad90842f
zepatrik <zepatrik@users.noreply.github.com>
2021-01-08 16:08:17 +0100

    export ConditionalPrinter

--- cmdx/noise_printer.go
+++ cmdx/noise_printer.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/pflag"
 )
 
-type conditionalPrinter struct {
+type ConditionalPrinter struct {
 	w     io.Writer
 	print bool
 }
@@ -21,112 +21,112 @@ func RegisterNoiseFlags(flags *pflag.FlagSet) {
 	flags.BoolP(FlagQuiet, FlagQuiet[:1], false, "Be quiet with output printing.")
 }
 
-// NewLoudOutPrinter returns a conditionalPrinter that
+// NewLoudOutPrinter returns a ConditionalPrinter that
 // only prints to cmd.OutOrStdout when --quiet is not set
-func NewLoudOutPrinter(cmd *cobra.Command) *conditionalPrinter {
+func NewLoudOutPrinter(cmd *cobra.Command) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     cmd.OutOrStdout(),
 		print: !quiet,
 	}
 }
 
-// NewQuietOutPrinter returns a conditionalPrinter that
+// NewQuietOutPrinter returns a ConditionalPrinter that
 // only prints to cmd.OutOrStdout when --quiet is set
-func NewQuietOutPrinter(cmd *cobra.Command) *conditionalPrinter {
+func NewQuietOutPrinter(cmd *cobra.Command) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     cmd.OutOrStdout(),
 		print: quiet,
 	}
 }
 
-// NewLoudErrPrinter returns a conditionalPrinter that
+// NewLoudErrPrinter returns a ConditionalPrinter that
 // only prints to cmd.ErrOrStderr when --quiet is not set
-func NewLoudErrPrinter(cmd *cobra.Command) *conditionalPrinter {
+func NewLoudErrPrinter(cmd *cobra.Command) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     cmd.ErrOrStderr(),
 		print: !quiet,
 	}
 }
 
-// NewQuietErrPrinter returns a conditionalPrinter that
+// NewQuietErrPrinter returns a ConditionalPrinter that
 // only prints to cmd.ErrOrStderr when --quiet is set
-func NewQuietErrPrinter(cmd *cobra.Command) *conditionalPrinter {
+func NewQuietErrPrinter(cmd *cobra.Command) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     cmd.ErrOrStderr(),
 		print: quiet,
 	}
 }
 
-// NewLoudPrinter returns a conditionalPrinter that
+// NewLoudPrinter returns a ConditionalPrinter that
 // only prints to w when --quiet is not set
-func NewLoudPrinter(cmd *cobra.Command, w io.Writer) *conditionalPrinter {
+func NewLoudPrinter(cmd *cobra.Command, w io.Writer) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     w,
 		print: !quiet,
 	}
 }
 
-// NewQuietPrinter returns a conditionalPrinter that
+// NewQuietPrinter returns a ConditionalPrinter that
 // only prints to w when --quiet is set
-func NewQuietPrinter(cmd *cobra.Command, w io.Writer) *conditionalPrinter {
+func NewQuietPrinter(cmd *cobra.Command, w io.Writer) *ConditionalPrinter {
 	quiet, err := cmd.Flags().GetBool(FlagQuiet)
 	if err != nil {
 		Fatalf(err.Error())
 	}
 
-	return &conditionalPrinter{
+	return &ConditionalPrinter{
 		w:     w,
 		print: quiet,
 	}
 }
 
-func NewConditionalPrinter(w io.Writer, print bool) *conditionalPrinter {
-	return &conditionalPrinter{
+func NewConditionalPrinter(w io.Writer, print bool) *ConditionalPrinter {
+	return &ConditionalPrinter{
 		w:     w,
 		print: print,
 	}
 }
 
-func (p *conditionalPrinter) Println(a ...interface{}) (n int, err error) {
+func (p *ConditionalPrinter) Println(a ...interface{}) (n int, err error) {
 	if p.print {
 		return fmt.Fprintln(p.w, a...)
 	}
 	return
 }
 
-func (p *conditionalPrinter) Print(a ...interface{}) (n int, err error) {
+func (p *ConditionalPrinter) Print(a ...interface{}) (n int, err error) {
 	if p.print {
 		return fmt.Fprint(p.w, a...)
 	}
 	return
 }
 
-func (p *conditionalPrinter) Printf(format string, a ...interface{}) (n int, err error) {
+func (p *ConditionalPrinter) Printf(format string, a ...interface{}) (n int, err error) {
 	if p.print {
 		return fmt.Fprintf(p.w, format, a...)
 	}
