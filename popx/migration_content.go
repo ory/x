48fd667ce299872e1a4e00d2406e5a702e2c262c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-02-17 11:17:28 +0100

    feat(popx): reliable migrations

---
+++ popx/migration_content.go
@@ -0,0 +1,56 @@
+package popx
+
+import (
+	"bytes"
+	"text/template"
+
+	"github.com/gobuffalo/fizz"
+	"github.com/gobuffalo/pop/v5"
+	"github.com/pkg/errors"
+)
+
+func ParameterizedMigrationContent(params map[string]interface{}) func(mf Migration, c *pop.Connection, r []byte, usingTemplate bool) (string, error) {
+	return func(mf Migration, c *pop.Connection, b []byte, usingTemplate bool) (string, error) {
+		content := ""
+		if usingTemplate {
+			t, err := template.New("migration").Parse(string(b))
+			if err != nil {
+				return "", errors.Wrapf(err, "could not parse template %s", mf.Path)
+			}
+			var bb bytes.Buffer
+			err = t.Execute(&bb, struct {
+				IsSQLite       bool
+				IsCockroach    bool
+				IsMySQL        bool
+				IsMariaDB      bool
+				IsPostgreSQL   bool
+				DialectDetails *pop.ConnectionDetails
+				Parameters     map[string]interface{}
+			}{
+				IsSQLite:       c.Dialect.Name() == "sqlite3",
+				IsCockroach:    c.Dialect.Name() == "cockroach",
+				IsMySQL:        c.Dialect.Name() == "mysql",
+				IsMariaDB:      c.Dialect.Name() == "mariadb",
+				IsPostgreSQL:   c.Dialect.Name() == "postgres",
+				DialectDetails: c.Dialect.Details(),
+				Parameters:     params,
+			})
+			if err != nil {
+				return "", errors.Wrapf(err, "could not execute migration template %s", mf.Path)
+			}
+			content = bb.String()
+		} else {
+			content = string(b)
+		}
+
+		if mf.Type == "fizz" {
+			var err error
+			content, err = fizz.AString(content, c.Dialect.FizzTranslator())
+			if err != nil {
+				return "", errors.Wrapf(err, "could not fizz the migration %s", mf.Path)
+			}
+		}
+
+		return content, nil
+	}
+}
---
+++ popx/migration_info_test.go
@@ -0,0 +1,51 @@
+package popx
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSortingMigrations(t *testing.T) {
+	t.Run("case=enforces precedence for specific migrations", func(t *testing.T) {
+		migrations := Migrations{
+			{
+				Version: "1",
+				DBType:  "all",
+			},
+			{
+				Version: "1",
+				DBType:  "postgres",
+			},
+			{
+				Version: "2",
+				DBType:  "cockroach",
+			},
+			{
+				Version: "2",
+				DBType:  "all",
+			},
+			{
+				Version: "3",
+				DBType:  "all",
+			},
+			{
+				Version: "3",
+				DBType:  "mysql",
+			},
+		}
+		expectedOrder := Migrations{
+			migrations[1],
+			migrations[0],
+			migrations[2],
+			migrations[3],
+			migrations[5],
+			migrations[4],
+		}
+
+		sort.Sort(migrations)
+
+		assert.Equal(t, expectedOrder, migrations)
+	})
+}
