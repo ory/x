753ef8ba00712b75812e50ef1e7f98afd4f54922
zepatrik <zepatrik@users.noreply.github.com>
2021-03-08 11:47:17 +0100

    fix: var name clash

--- popx/migration_box_template_test.go
+++ popx/migration_box_template_test.go
@@ -13,14 +13,14 @@ import (
 )
 
 //go:embed stub/migrations/templating/*.sql
-var migrations embed.FS
+var templatingMigrations embed.FS
 
 func TestMigrationBoxTemplating(t *testing.T) {
 	templateVals := map[string]interface{}{
 		"tableName": "test_table_name",
 	}
 
-	expectedMigration, err := migrations.ReadFile("stub/migrations/templating/0_sql_create_tablename_template.expected.sql")
+	expectedMigration, err := templatingMigrations.ReadFile("stub/migrations/templating/0_sql_create_tablename_template.expected.sql")
 	require.NoError(t, err)
 
 	c, err := pop.NewConnection(&pop.ConnectionDetails{
@@ -29,7 +29,7 @@ func TestMigrationBoxTemplating(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, c.Open())
 
-	_, err = NewMigrationBox(migrations, NewMigrator(c, logrusx.New("", ""), nil, 0), WithTemplateValues(templateVals), WithMigrationContentMiddleware(func(content string, err error) (string, error) {
+	_, err = NewMigrationBox(templatingMigrations, NewMigrator(c, logrusx.New("", ""), nil, 0), WithTemplateValues(templateVals), WithMigrationContentMiddleware(func(content string, err error) (string, error) {
 		require.NoError(t, err)
 		assert.Equal(t, string(expectedMigration), content)
 
