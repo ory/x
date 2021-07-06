d94d1295c83df872be16766d2b18557ab776b367
Patrik <zepatrik@users.noreply.github.com>
2021-07-06 15:52:13 +0200

    fix(fsx): make merged file system walkable and fully comply with "testing/fstest" (#370)

--- fsx/merge_test.go
+++ fsx/merge_test.go
@@ -1,28 +1,22 @@
 package fsx
 
 import (
-	"embed"
-	"io/fs"
 	"testing"
-
-	"github.com/pkg/errors"
+	"testing/fstest"
 
 	"github.com/stretchr/testify/assert"
 )
 
-//go:embed merge.go
-var prodFS embed.FS
-
-//go:embed merge_test.go
-var testFS embed.FS
-
 func TestMergeFS(t *testing.T) {
-	mergedFS := Merge(prodFS, testFS)
+	a := fstest.MapFS{
+		"a":     &fstest.MapFile{},
+		"dir/c": &fstest.MapFile{},
+	}
+	b := fstest.MapFS{
+		"b":     &fstest.MapFile{},
+		"dir/d": &fstest.MapFile{},
+	}
+	m := Merge(a, b)
 
-	_, err := mergedFS.Open("merge.go")
-	assert.NoError(t, err)
-	_, err = mergedFS.Open("merge_test.go")
-	assert.NoError(t, err)
-	_, err = mergedFS.Open("unknown file")
-	assert.True(t, errors.Is(err, fs.ErrNotExist))
+	assert.NoError(t, fstest.TestFS(m, "a", "b", "dir", "dir/c", "dir/d"))
 }
