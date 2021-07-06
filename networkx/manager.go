91a9a2951be920ff83b838d1cb6ec5d1fce6eb4d
zepatrik <zepatrik@users.noreply.github.com>
2021-07-06 10:35:09 +0200

    docs: add comment to exported var

--- networkx/manager.go
+++ networkx/manager.go
@@ -13,6 +13,8 @@ import (
 	"github.com/ory/x/tracing"
 )
 
+// Migrations of the network manager. Apply by merging with your local migrations using
+// fsx.Merge() and then passing all to the migration box.
 //go:embed migrations/sql/*.sql
 var Migrations embed.FS
 
