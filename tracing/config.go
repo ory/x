1182cbb3f2e3bae58787356dab77f86291db1eec
zepatrik <zepatrik@users.noreply.github.com>
2021-07-20 13:13:40 +0200

    feat(tracing): add tracing config schema From now on we can use `"$ref": "https://ory.sh/schemas/tracing.schema.json"` in the config schemas instead of having to copy it around.

--- tracing/config.go
+++ tracing/config.go
@@ -1,23 +1,58 @@
 package tracing
 
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"io"
+)
+
 // JaegerConfig encapsulates jaeger's configuration.
 type JaegerConfig struct {
-	LocalAgentHostPort string
-	SamplerType        string
-	SamplerValue       float64
-	SamplerServerURL   string
-	Propagation        string
-	MaxTagValueLength  int
+	LocalAgentAddress string          `json:"local_agent_address"`
+	Sampling          *JaegerSampling `json:"sampling"`
+	Propagation       string          `json:"propagation"`
+	MaxTagValueLength int             `json:"max_tag_value_length"`
+}
+
+type JaegerSampling struct {
+	Type      string  `json:"type"`
+	Value     float64 `json:"value"`
+	ServerURL string  `json:"server_url"`
 }
 
 // ZipkinConfig encapsulates zipkin's configuration.
 type ZipkinConfig struct {
-	ServerURL string
+	ServerURL string `json:"server_url"`
 }
 
 type Config struct {
-	ServiceName string
-	Provider    string
-	Jaeger      *JaegerConfig
-	Zipkin      *ZipkinConfig
+	ServiceName string           `json:"service_name"`
+	Provider    string           `json:"provider"`
+	Providers   *ProvidersConfig `json:"providers"`
+}
+
+type ProvidersConfig struct {
+	Jaeger *JaegerConfig `json:"jaeger"`
+	Zipkin *ZipkinConfig `json:"zipkin"`
+}
+
+//go:embed config.schema.json
+var ConfigSchema embed.FS
+
+const ConfigSchemaID = "https://ory.sh/schemas/tracing.schema.json"
+
+// AddConfigSchema adds the tracing schema to the compiler.
+// The interface is specified instead of `jsonschema.Compiler` to allow the use of any jsonschema library fork or version.
+func AddConfigSchema(c interface {
+	AddResource(url string, r io.Reader) error
+}, serviceName string) error {
+	s, err := ConfigSchema.ReadFile("config.schema.json")
+	if err != nil {
+		panic("unrecoverable error: could not read file from embed.FS: " + err.Error())
+	}
+
+	finalSchema := fmt.Sprintf(string(s), serviceName)
+
+	return c.AddResource(ConfigSchemaID, bytes.NewBufferString(finalSchema))
 }
---
+++ tracing/config_test.go
@@ -0,0 +1,59 @@
+package tracing
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/sjson"
+
+	"github.com/ory/jsonschema/v3"
+)
+
+const rootSchema = `{
+  "properties": {
+    "tracing": {
+      "$ref": "%s"
+    }
+  }
+}
+`
+
+func TestTracing(t *testing.T) {
+	t.Run("func=AddConfigSchema", func(t *testing.T) {
+		c := jsonschema.NewCompiler()
+		require.NoError(t, AddConfigSchema(c, "Ory X"))
+
+		conf := Config{
+			ServiceName: "Ory X",
+			Provider:    "jaeger",
+			Providers: &ProvidersConfig{
+				Jaeger: &JaegerConfig{
+					LocalAgentAddress: "jaeger:6831",
+					Sampling: &JaegerSampling{
+						Type:      "const",
+						Value:     1,
+						ServerURL: "https://localhost:5778/sampling",
+					},
+					Propagation:       "jaeger",
+					MaxTagValueLength: 100,
+				},
+				Zipkin: &ZipkinConfig{
+					ServerURL: "https://example.com",
+				},
+			},
+		}
+
+		rawConfig, err := sjson.Set("{}", "tracing", &conf)
+		require.NoError(t, err)
+
+		require.NoError(t, c.AddResource("config", bytes.NewBufferString(fmt.Sprintf(rootSchema, ConfigSchemaID))))
+
+		schema, err := c.Compile("config")
+		require.NoError(t, err)
+
+		assert.NoError(t, schema.Validate(bytes.NewBufferString(rawConfig)))
+	})
+}
