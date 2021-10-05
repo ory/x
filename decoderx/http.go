46cab047be65926b59d20098fc6d2dd092512af1
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-10-05 13:12:39 +0200

    feat: add required detection to jsonschema
    See https://github.com/ory/kratos/issues/400
    See https://github.com/ory-corp/cloud/issues/1328
    See https://github.com/ory/kratos/issues/1058
    See https://ory-community.slack.com/archives/C012RJ2MQ1H/p1631825476159000

--- decoderx/http.go
+++ decoderx/http.go
@@ -455,6 +455,13 @@ func (t *HTTP) decodeURLValues(values url.Values, paths []jsonschemax.Path, o *h
 					raw, err = sjson.SetBytes(raw, path.Name, values[key])
 				case bool:
 					v := values[key][len(values[key])-1]
+					if len(v) == 0 {
+						if !path.Required {
+							continue
+						}
+						v = "false"
+					}
+
 					if f, err := strconv.ParseBool(v); err != nil {
 						switch o.handleParseErrors {
 						case ParseErrorIgnoreConversionErrors:
@@ -472,6 +479,13 @@ func (t *HTTP) decodeURLValues(values url.Values, paths []jsonschemax.Path, o *h
 					}
 				case float64:
 					v := values.Get(key)
+					if len(v) == 0 {
+						if !path.Required {
+							continue
+						}
+						v = "0.0"
+					}
+
 					if f, err := strconv.ParseFloat(v, 64); err != nil {
 						switch o.handleParseErrors {
 						case ParseErrorIgnoreConversionErrors:
@@ -488,9 +502,19 @@ func (t *HTTP) decodeURLValues(values url.Values, paths []jsonschemax.Path, o *h
 						raw, err = sjson.SetBytes(raw, path.Name, f)
 					}
 				case string:
-					raw, err = sjson.SetBytes(raw, path.Name, values.Get(key))
+					v := values.Get(key)
+					if len(v) == 0 {
+						continue
+					}
+
+					raw, err = sjson.SetBytes(raw, path.Name, v)
 				case map[string]interface{}:
-					raw, err = sjson.SetBytes(raw, path.Name, values.Get(key))
+					v := values.Get(key)
+					if len(v) == 0 && !path.Required {
+						continue
+					}
+
+					raw, err = sjson.SetBytes(raw, path.Name, v)
 				case []map[string]interface{}:
 					raw, err = sjson.SetBytes(raw, path.Name, values[key])
 				}
