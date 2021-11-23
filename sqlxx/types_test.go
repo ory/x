af10b8cabcd70f58ba935da95a0efe208089be75
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-11-23 11:02:19 +0100

    feat(sqlxx): add nullbull

--- sqlxx/types_test.go
+++ sqlxx/types_test.go
@@ -2,6 +2,7 @@ package sqlxx
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,6 +20,74 @@ func TestNullString_UnmarshalJSON(t *testing.T) {
 	var ns NullString
 	require.NoError(t, json.Unmarshal(data, &ns))
 	assert.EqualValues(t, "hello", ns)
+}
+
+func TestNullBoolMarshalJSON(t *testing.T) {
+	type outer struct {
+		Bool *NullBool `json:"null_bool,omitempty"`
+	}
+
+	for k, tc := range []struct {
+		in       *outer
+		expected string
+	}{
+		{in: &outer{&NullBool{Valid: false, Bool: true}}, expected: "{\"null_bool\":null}"},
+		{in: &outer{&NullBool{Valid: true, Bool: true}}, expected: "{\"null_bool\":true}"},
+		{in: &outer{&NullBool{Valid: true, Bool: false}}, expected: "{\"null_bool\":false}"},
+		{in: &outer{}, expected: "{}"},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			out, err := json.Marshal(tc.in)
+			require.NoError(t, err)
+			assert.EqualValues(t, tc.expected, string(out))
+		})
+	}
+}
+
+func TestNullBoolUnMarshalJSONNoPointer(t *testing.T) {
+	type outer struct {
+		Bool NullBool `json:"null_bool,omitempty"`
+	}
+
+	for k, tc := range []struct {
+		expected outer
+		in       string
+	}{
+		{expected: outer{}, in: "{}"},
+		{expected: outer{NullBool{Valid: true, Bool: true}}, in: "{\"null_bool\":true}"},
+		{expected: outer{NullBool{Valid: true, Bool: false}}, in: "{\"null_bool\":false}"},
+		{expected: outer{NullBool{}}, in: "{\"null_bool\":null}"},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			var actual outer
+			err := json.Unmarshal([]byte(tc.in), &actual)
+			require.NoError(t, err)
+			assert.EqualValues(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestNullBoolUnMarshalJSON(t *testing.T) {
+	type outer struct {
+		Bool *NullBool `json:"null_bool,omitempty"`
+	}
+
+	for k, tc := range []struct {
+		expected outer
+		in       string
+	}{
+		{expected: outer{}, in: "{}"},
+		{expected: outer{&NullBool{Valid: true, Bool: true}}, in: "{\"null_bool\":true}"},
+		{expected: outer{&NullBool{Valid: true, Bool: false}}, in: "{\"null_bool\":false}"},
+		{expected: outer{}, in: "{\"null_bool\":null}"},
+	} {
+		t.Run(fmt.Sprintf("case=%d", k), func(t *testing.T) {
+			var actual outer
+			err := json.Unmarshal([]byte(tc.in), &actual)
+			require.NoError(t, err)
+			assert.EqualValues(t, tc.expected, actual)
+		})
+	}
 }
 
 func TestStringSlicePipeDelimiter(t *testing.T) {
