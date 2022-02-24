e7796668c8bfbb1c378465daa3da783845f651c0
nipsufn <17983323+nipsufn@users.noreply.github.com>
2022-02-24 20:24:29 +0100

    add test

--- httpx/resilient_client_test.go
+++ httpx/resilient_client_test.go
@@ -41,6 +41,40 @@ func TestNoPrivateIPs(t *testing.T) {
 	}
 }
 
+// TODO: test various exceptions, not just localhost
+func TestNoPrivateIPsExceptions(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Hello, world!"))
+	}))
+	t.Cleanup(ts.Close)
+	c := NewResilientClient(
+		ResilientClientWithMaxRetry(1),
+		ResilientClientDisallowInternalIPs(),
+		ResilientClientDisallowExceptions([]string{"localhost"}))
+
+	target, err := url.ParseRequestURI(ts.URL)
+	require.NoError(t, err)
+
+	_, port, err := net.SplitHostPort(target.Host)
+	require.NoError(t, err)
+
+	for _, host := range []string{
+		"127.0.0.1",
+		"localhost",
+		"192.168.178.5",
+	} {
+		target.Host = host + ":" + port
+		t.Logf("%s", target.String())
+		_, err := c.Get(target.String())
+		if host != "localhost" {
+			require.Error(t, err)
+			assert.Contains(t, err.Error(), "is in the")
+		} else {
+			assert.Equal(t, err, nil)
+		}
+	}
+}
+
 func TestClientWithTracer(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write([]byte("Hello, world!"))
