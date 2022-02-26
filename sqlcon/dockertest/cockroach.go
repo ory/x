c6855827bad81516a1cd3c8b75a39c65e8377f82
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-26 11:41:37 +0100

    feat(sqlcon): add local cockroach helper

---
+++ sqlcon/dockertest/cockroach.go
@@ -0,0 +1,19 @@
+package dockertest
+
+import (
+	"testing"
+
+	"github.com/cockroachdb/cockroach-go/v2/testserver"
+	"github.com/stretchr/testify/require"
+)
+
+func NewLocalTestCRDBServer(t testing.TB) string {
+	ts, err := testserver.NewTestServer()
+	require.NoError(t, err)
+	t.Cleanup(ts.Stop)
+
+	require.NoError(t, ts.WaitForInit())
+
+	ts.PGURL().Scheme = "cockroach"
+	return ts.PGURL().String()
+}
