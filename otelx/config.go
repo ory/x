95b83cb7948b2b8973cfe7e2df405a9a82e60a78
Anirudh Oppiliappan <x@icyphox.sh>
2022-03-14 11:19:20 +0530

    test: config schema

--- otelx/config.go
+++ otelx/config.go
@@ -33,7 +33,7 @@ type Config struct {
 //go:embed config.schema.json
 var ConfigSchema string
 
-const ConfigSchemaID = "ory://tracing-config"
+const ConfigSchemaID = "ory://otelx-config"
 
 // AddConfigSchema adds the tracing schema to the compiler.
 // The interface is specified instead of `jsonschema.Compiler` to allow the use of any jsonschema library fork or version.
