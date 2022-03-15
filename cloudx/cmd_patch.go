921fb099a6aa091ccd5443f27df18e7ba4279382
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-15 15:43:09 +0100

    feat(cloudx): implement patch project

---
+++ cloudx/cmd_patch.go
@@ -0,0 +1,17 @@
+package cloudx
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func NewPatchCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "patch",
+		Short: fmt.Sprintf("Patch resources"),
+	}
+	RegisterConfigFlag(cmd.PersistentFlags())
+	cmd.AddCommand(NewProjectsPatchCmd())
+	return cmd
+}
