f50045f272b411c9150e2972cc7098d6676bfbb7
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-03-24 16:21:19 +0100

    docs: update command docs

--- cloudx/cmd_get_kratos_config.go
+++ cloudx/cmd_get_kratos_config.go
@@ -1,8 +1,6 @@
 package cloudx
 
 import (
-	"fmt"
-
 	"github.com/ory/x/cmdx"
 
 	"github.com/spf13/cobra"
@@ -13,9 +11,20 @@ func NewGetKratosConfigCmd() *cobra.Command {
 		Use:     "identity-config project-id",
 		Aliases: []string{"ic", "kratos-config"},
 		Args:    cobra.ExactArgs(1),
-		Short:   fmt.Sprintf("Get an Ory Cloud project and output it as Ory Kratos configuration"),
-		Example: `ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format yaml > kratos-config.yaml
-ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format json > kratos-config.json`,
+		Short:   "Get an Ory Cloud project's identity configuration",
+		Long:    "You can use this command to render Ory Kratos configurations as well.",
+		Example: `$ ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format yaml > kratos-config.yaml
+
+$ ory get kratos-config ecaaa3cb-0730-4ee8-a6df-9553cdfeef89 --format json
+
+{
+  "selfservice": {
+	"methods": {
+	  "password": { "enabled": false }
+	}
+	// ...
+  }
+}`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			h, err := NewSnakeCharmer(cmd)
 			if err != nil {
