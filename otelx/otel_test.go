58172c30fcadfbf6aa1f8599ea303d373cda8afa
Anirudh Oppiliappan <x@icyphox.sh>
2022-04-14 17:59:47 +0530

    test: improve jaeger tracer tests

--- otelx/otel_test.go
+++ otelx/otel_test.go
@@ -5,23 +5,42 @@ import (
 	"fmt"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/ory/x/logrusx"
+	"github.com/phayes/freeport"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/otel/attribute"
+	"golang.org/x/sync/errgroup"
 )
 
 func TestJaegerTracer(t *testing.T) {
-	host := "127.0.0.1"
-	port := "6831"
 	done := make(chan struct{})
-	go func(addr string) {
-		t.Log("Starting test UDP server for Jaeger spans")
-		udpAddr, _ := net.ResolveUDPAddr("udp", addr)
-		srv, _ := net.ListenUDP("udp", udpAddr)
+	port, err := freeport.GetFreePort()
+	require.NoError(t, err)
+	addr := fmt.Sprintf("127.0.0.1:%d", port)
+
+	errs := errgroup.Group{}
+	errs.Go(func() error {
+		udpAddr, err := net.ResolveUDPAddr("udp", addr)
+		if err != nil {
+			return err
+		}
+
+		t.Logf("Starting test UDP server for Jaeger spans on %s", udpAddr.String())
+
+		srv, err := net.ListenUDP("udp", udpAddr)
+		if err != nil {
+			return err
+		}
+
 		for {
 			buf := make([]byte, 2048)
-			_, conn, _ := srv.ReadFromUDP(buf)
+			_, conn, err := srv.ReadFromUDP(buf)
+			if err != nil {
+				return err
+			}
+
 			if conn == nil {
 				continue
 			}
@@ -31,14 +50,15 @@ func TestJaegerTracer(t *testing.T) {
 			}
 			break
 		}
-	}(fmt.Sprintf("%s:%s", host, port))
+		return nil
+	})
 
 	ot, err := New("github.com/ory/x/otelx", logrusx.New("ory/x", "1"), &Config{
 		ServiceName: "Ory X",
 		Provider:    "jaeger",
 		Providers: ProvidersConfig{
 			Jaeger: JaegerConfig{
-				LocalAgentAddress: "127.0.0.1:6831",
+				LocalAgentAddress: addr,
 			},
 		},
 	})
@@ -49,5 +69,11 @@ func TestJaegerTracer(t *testing.T) {
 	span.SetAttributes(attribute.Bool("testAttribute", true))
 	span.End()
 
-	<-done
+	select {
+	case <-done:
+	case <-time.After(15 * time.Second):
+		t.Log("expected to receive span, but did not receive any")
+		t.Fail()
+	}
+	require.NoError(t, errs.Wait())
 }
