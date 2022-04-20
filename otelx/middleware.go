fea7fea33281f8690287dcdd3512ce8ab2d44d1e
Anirudh Oppiliappan <x@icyphox.sh>
2022-04-20 15:43:17 +0530

    chore: remove WrapHTTPRouter

--- otelx/middleware.go
+++ otelx/middleware.go
@@ -4,12 +4,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/julienschmidt/httprouter"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
-	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
-	"go.opentelemetry.io/otel/trace"
 )
 
 const tracingComponent = "github.com/ory/x/otelx"
@@ -35,32 +30,4 @@ func filterOpts() []otelhttp.Option {
 
 func NewHandler(handler http.Handler, operation string) http.Handler {
 	return otelhttp.NewHandler(handler, operation, filterOpts()...)
-}
-
-// Middleware to satisfy httprouter.Handle. The URL path is used as
-// the span name.
-func WrapHTTPRouter(next httprouter.Handle) httprouter.Handle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		operation := r.URL.Path
-		var tracer trace.Tracer
-		if span := trace.SpanFromContext(r.Context()); span.SpanContext().IsValid() {
-			tracer = span.TracerProvider().Tracer(tracingComponent)
-		} else {
-			tracer = otel.GetTracerProvider().Tracer(tracingComponent)
-		}
-
-		opts := append([]trace.SpanStartOption{
-			trace.WithAttributes(semconv.NetAttributesFromHTTPRequest("tcp", r)...),
-			trace.WithAttributes(semconv.EndUserAttributesFromHTTPRequest(r)...),
-			trace.WithAttributes(semconv.HTTPServerAttributesFromHTTPRequest(operation, "", r)...),
-		})
-
-		ctx := otel.GetTextMapPropagator().Extract(r.Context(), propagation.HeaderCarrier(r.Header))
-		_, span := tracer.Start(ctx, operation, opts...)
-		span.SetName(operation)
-		defer span.End()
-
-		r = r.WithContext(ctx)
-		next(w, r, ps)
-	}
 }
