bf9949b4faf13fa16feb767f5ebac97e776df7cc
Anirudh Oppiliappan <x@icyphox.sh>
2022-05-17 18:16:56 +0530

    feat(otelx): ignore admin health endpoint

--- otelx/middleware.go
+++ otelx/middleware.go
@@ -17,9 +17,18 @@ func isHealthFilter(r *http.Request) bool {
 	return true
 }
 
+func isAdminHealthFilter(r *http.Request) bool {
+	path := r.URL.Path
+	if strings.HasPrefix(path, "/admin/health/") {
+		return false
+	}
+	return true
+}
+
 func filterOpts() []otelhttp.Option {
 	filters := []otelhttp.Filter{
 		isHealthFilter,
+		isAdminHealthFilter,
 	}
 	opts := []otelhttp.Option{}
 	for _, f := range filters {
