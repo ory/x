2a3459489215963c36715be318c8e400c5469f2b
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-05-24 14:08:37 +0200

    test: healthx middleware

--- healthx/handler_test.go
+++ healthx/handler_test.go
@@ -36,73 +36,10 @@ import (
 	"github.com/ory/herodot"
 )
 
-const mockHeaderKey = "middleware-header"
-const mockHeaderValue = "test-header-value"
-
-func coreHealthTests(t *testing.T, handler *Handler, router *httprouter.Router, withMiddleware bool) {
-
-	alive := errors.New("not alive")
-
-	handler.ReadyChecks = map[string]ReadyChecker{
-		"test": func(r *http.Request) error {
-			return alive
-		},
-	}
-
-	// helper to run the same assertion on various responses that are with the middleware,
-	// avoid having to write the same if condition and assertion again
-	assertMockHeader := func(t *testing.T, res *http.Response, withMiddleware bool) {
-		if withMiddleware == false {
-			return
-		}
-		assert.EqualValues(t, mockHeaderValue, res.Header.Get(mockHeaderKey))
-	}
-
-	ts := httptest.NewServer(router)
-
-	c := http.DefaultClient
-
-	var healthBody swaggerHealthStatus
-	response, err := c.Get(ts.URL + AliveCheckPath)
-	require.NoError(t, err)
-	require.EqualValues(t, http.StatusOK, response.StatusCode)
-	require.NoError(t, json.NewDecoder(response.Body).Decode(&healthBody))
-	assert.EqualValues(t, "ok", healthBody.Status)
-	assertMockHeader(t, response, withMiddleware)
-
-	var versionBody swaggerVersion
-	response, err = c.Get(ts.URL + VersionPath)
-	require.NoError(t, err)
-	require.EqualValues(t, http.StatusOK, response.StatusCode)
-	require.NoError(t, json.NewDecoder(response.Body).Decode(&versionBody))
-	require.EqualValues(t, versionBody.Version, handler.VersionString)
-	assertMockHeader(t, response, withMiddleware)
-
-	response, err = c.Get(ts.URL + ReadyCheckPath)
-	require.NoError(t, err)
-	require.EqualValues(t, http.StatusServiceUnavailable, response.StatusCode)
-	out, err := ioutil.ReadAll(response.Body)
-	require.NoError(t, err)
-	assert.EqualValues(t, "ok", healthBody.Status)
-	assert.Equal(t, `{"errors":{"test":"not alive"}}`, strings.TrimSpace(string(out)))
-	assertMockHeader(t, response, withMiddleware)
-
-	alive = nil
-	response, err = c.Get(ts.URL + ReadyCheckPath)
-	require.NoError(t, err)
-	require.EqualValues(t, http.StatusOK, response.StatusCode)
-	require.NoError(t, json.NewDecoder(response.Body).Decode(&versionBody))
-	require.EqualValues(t, versionBody.Version, handler.VersionString)
-	assertMockHeader(t, response, withMiddleware)
-
-}
-
 func TestHealth(t *testing.T) {
-
-	handler := &Handler{
-		H:             herodot.NewJSONWriter(nil),
-		VersionString: "test version",
-	}
+	const mockHeaderKey = "middleware-header"
+	const mockHeaderValue = "test-header-value"
+	const mockVersion = "test version"
 
 	// middlware to run an assert function on the requested handler
 	testMiddleware := func(t *testing.T, assertFunc func(*testing.T, http.ResponseWriter, *http.Request)) func(next http.Handler) http.Handler {
@@ -115,18 +52,140 @@ func TestHealth(t *testing.T) {
 		}
 	}
 
-	t.Run("case:without middleware", func(t *testing.T) {
+	assertAliveCheck := func(t *testing.T, endpoint string, handler *Handler) *http.Response {
+		var healthBody swaggerHealthStatus
+		c := http.DefaultClient
+		response, err := c.Get(endpoint)
+		require.NoError(t, err)
+		require.EqualValues(t, http.StatusOK, response.StatusCode)
+		require.NoError(t, json.NewDecoder(response.Body).Decode(&healthBody))
+		assert.EqualValues(t, "ok", healthBody.Status)
+		return response
+	}
+
+	assertVersionResponse := func(t *testing.T, endpoint string, handler *Handler) *http.Response {
+		var versionBody swaggerVersion
+		c := http.DefaultClient
+		response, err := c.Get(endpoint)
+		require.NoError(t, err)
+		require.EqualValues(t, http.StatusOK, response.StatusCode)
+		require.NoError(t, json.NewDecoder(response.Body).Decode(&versionBody))
+		require.EqualValues(t, mockVersion, versionBody.Version)
+		return response
+	}
+
+	assertReadyCheckNotAlive := func(t *testing.T, endpoint string, handler *Handler) *http.Response {
+		handler.ReadyChecks = map[string]ReadyChecker{
+			"test": func(r *http.Request) error {
+				return errors.New("not alive")
+			},
+		}
+		c := http.DefaultClient
+		response, err := c.Get(endpoint)
+		require.NoError(t, err)
+		require.EqualValues(t, http.StatusServiceUnavailable, response.StatusCode)
+		out, err := ioutil.ReadAll(response.Body)
+		require.NoError(t, err)
+		assert.Equal(t, `{"errors":{"test":"not alive"}}`, strings.TrimSpace(string(out)))
+		return response
+	}
+
+	assertReadyCheck := func(t *testing.T, endpoint string, handler *Handler) *http.Response {
+		handler.ReadyChecks = map[string]ReadyChecker{
+			"test": func(r *http.Request) error {
+				return nil
+			},
+		}
+		var healthCheck swaggerHealthStatus
+		c := http.DefaultClient
+		response, err := c.Get(endpoint)
+		require.NoError(t, err)
+		require.EqualValues(t, http.StatusOK, response.StatusCode)
+		require.NoError(t, json.NewDecoder(response.Body).Decode(&healthCheck))
+		require.EqualValues(t, swaggerHealthStatus{Status: "ok"}, healthCheck)
+		return response
+	}
+
+	testCases := []struct {
+		description string
+		url         func(mockServerURL string) string
+		test        func(t *testing.T, endpoint string, handler *Handler) *http.Response
+	}{
+		{
+			description: "ready check should return status ok",
+			url: func(mockServerURL string) string {
+				return mockServerURL + ReadyCheckPath
+			},
+			test: assertReadyCheck,
+		},
+		{
+			description: "ready check should return error",
+			url: func(mockServerURL string) string {
+				return mockServerURL + ReadyCheckPath
+			},
+			test: assertReadyCheckNotAlive,
+		},
+		{
+			description: "alive check should return status ok",
+			url: func(mockServerURL string) string {
+				return mockServerURL + AliveCheckPath
+			},
+			test: assertAliveCheck,
+		},
+		{
+			description: "version should return",
+			url: func(mockServerURL string) string {
+				return mockServerURL + VersionPath
+			},
+			test: assertVersionResponse,
+		},
+	}
+
+	t.Run("case=without middleware", func(t *testing.T) {
 		router := httprouter.New()
+
+		handler := &Handler{
+			H:             herodot.NewJSONWriter(nil),
+			VersionString: mockVersion,
+			ReadyChecks: map[string]ReadyChecker{
+				"test": func(r *http.Request) error {
+					return nil
+				},
+			},
+		}
+
+		ts := httptest.NewServer(router)
+		defer ts.Close()
 
 		handler.SetHealthRoutes(router, true)
 		handler.SetVersionRoutes(router)
 
-		coreHealthTests(t, handler, router, false)
+		for _, tc := range testCases {
+			t.Run("case="+tc.description, func(t *testing.T) {
+				tc.test(t, tc.url(ts.URL), handler)
+			})
+		}
 	})
 
-	t.Run("case:with middleware", func(t *testing.T) {
+	t.Run("case=with middleware", func(t *testing.T) {
 		router := httprouter.New()
 
+		var alive error
+
+		handler := &Handler{
+			H:             herodot.NewJSONWriter(nil),
+			VersionString: mockVersion,
+			ReadyChecks: map[string]ReadyChecker{
+				"test": func(r *http.Request) error {
+					return alive
+				},
+			},
+		}
+
+		ts := httptest.NewServer(router)
+		defer ts.Close()
+
+		// set the health handlers with middleware
 		handler.SetHealthRoutes(router, true, WithMiddleware(
 			testMiddleware(t, func(t *testing.T, rw http.ResponseWriter, r *http.Request) {
 				assert.Equal(t, "GET", r.Method)
@@ -139,6 +198,11 @@ func TestHealth(t *testing.T) {
 			}),
 		))
 
-		coreHealthTests(t, handler, router, true)
+		for _, tc := range testCases {
+			t.Run("case="+tc.description, func(t *testing.T) {
+				response := tc.test(t, tc.url(ts.URL), handler)
+				assert.EqualValues(t, mockHeaderValue, response.Header.Get(mockHeaderKey))
+			})
+		}
 	})
 }
