c34f760eae6aee35289a19a82cd8fe8545bceca2
Anirudh Oppiliappan <x@icyphox.sh>
2022-05-26 15:06:09 +0530

    feat(otelx): add TraceHandler func

--- otelx/middleware.go
+++ otelx/middleware.go
@@ -37,6 +37,14 @@ func filterOpts() []otelhttp.Option {
 	return opts
 }
 
+// NewHandler returns a wrapped otelhttp.NewHandler with our request filters.
 func NewHandler(handler http.Handler, operation string) http.Handler {
 	return otelhttp.NewHandler(handler, operation, filterOpts()...)
+}
+
+// TraceHandler wraps otelx.NewHandler, passing the URL path as the span name.
+func TraceHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		NewHandler(h, r.URL.Path).ServeHTTP(w, r)
+	})
 }
