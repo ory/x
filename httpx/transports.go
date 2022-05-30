1ee4edf7fd526682858f31536df0cb3737b61432
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-05-30 18:32:33 +0200

    feat(httpx): add custom tranpsorters

---
+++ httpx/transports.go
@@ -0,0 +1,45 @@
+package httpx
+
+import "net/http"
+
+// NewTransportWithHeader returns a new http.Transport that always uses the values from the given header.
+func NewTransportWithHeader(h http.Header) *TransportWithHeader {
+	return &TransportWithHeader{
+		RoundTripper: http.DefaultTransport,
+		h:            h,
+	}
+}
+
+// TransportWithHeader is an http.RoundTripper that always uses the values from the given header.
+type TransportWithHeader struct {
+	http.RoundTripper
+	h http.Header
+}
+
+// RoundTrip implements http.RoundTripper.
+func (ct *TransportWithHeader) RoundTrip(req *http.Request) (*http.Response, error) {
+	for k := range ct.h {
+		req.Header.Set(k, ct.h.Get(k))
+	}
+	return ct.RoundTripper.RoundTrip(req)
+}
+
+// NewTransportWithHost returns a new http.Transport that always uses the given host.
+func NewTransportWithHost(host string) *TransportWithHost {
+	return &TransportWithHost{
+		RoundTripper: http.DefaultTransport,
+		host:         host,
+	}
+}
+
+// TransportWithHost is an http.RoundTripper that always uses the given host.
+type TransportWithHost struct {
+	http.RoundTripper
+	host string
+}
+
+// RoundTrip implements http.RoundTripper.
+func (ct *TransportWithHost) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Host = ct.host
+	return ct.RoundTripper.RoundTrip(req)
+}
