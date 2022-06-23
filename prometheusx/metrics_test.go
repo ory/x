bf04cdce76b69783762c66ac062e212a6d6e59e9
Ferdynand Naczynski <nipsufn+gh@pm.me>
2022-06-23 10:18:14 +0200

    feat: add endpoint label to http_requests_total metric and fix sjson flake (#537)

--- prometheusx/metrics_test.go
+++ prometheusx/metrics_test.go
@@ -54,10 +54,12 @@ func TestMetrics(t *testing.T) {
 
 	require.EqualValues(t, "http_requests_total", *text["http_requests_total"].Name)
 	require.EqualValues(t, "400", getLabelValue("code", text["http_requests_total"].Metric))
+	require.EqualValues(t, testPath, getLabelValue("endpoint", text["http_requests_total"].Metric))
 	require.EqualValues(t, testApp, getLabelValue("app", text["http_requests_total"].Metric))
 
 	require.EqualValues(t, "http_requests_duration_seconds", *text["http_requests_duration_seconds"].Name)
 	require.EqualValues(t, "400", getLabelValue("code", text["http_requests_duration_seconds"].Metric))
+	require.EqualValues(t, testPath, getLabelValue("endpoint", text["http_requests_duration_seconds"].Metric))
 	require.EqualValues(t, testApp, getLabelValue("app", text["http_requests_duration_seconds"].Metric))
 
 	require.EqualValues(t, "http_response_size_bytes", *text["http_response_size_bytes"].Name)
--- sjsonx/set_test.go
+++ sjsonx/set_test.go
@@ -1,20 +1,21 @@
 package sjsonx
 
 import (
+	"encoding/json"
+	"github.com/ory/x/assertx"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestSetBytes(t *testing.T) {
 	out, err := SetBytes([]byte(`{"a":1,"b":2,"c":3}`), map[string]interface{}{"d.e": "6", "d.f": "7"})
 	require.NoError(t, err)
-	assert.JSONEq(t, string(out), `{"a":1,"b":2,"c":3,"d":{"e":"6","f":"7"}}`)
+	assertx.EqualAsJSON(t, json.RawMessage(`{"a":1,"b":2,"c":3,"d":{"e":"6","f":"7"}}`), json.RawMessage(out))
 }
 
 func TestSet(t *testing.T) {
 	out, err := Set(`{"a":1,"b":2,"c":3}`, map[string]interface{}{"d.e": "6", "d.f": "7"})
 	require.NoError(t, err)
-	assert.JSONEq(t, out, `{"a":1,"b":2,"c":3,"d":{"e":"6","f":"7"}}`)
+	assertx.EqualAsJSON(t, json.RawMessage(`{"a":1,"b":2,"c":3,"d":{"e":"6","f":"7"}}`), json.RawMessage(out))
 }
