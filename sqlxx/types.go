715582c2629fcc8681908b4f4cd06ae8b97502e4
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-07-13 11:27:57 +0200

    feat: add tests and types

--- sqlxx/types.go
+++ sqlxx/types.go
@@ -400,7 +400,7 @@ func JSONValue(src interface{}) (driver.Value, error) {
 // swagger:model nullInt64
 type NullInt64 struct {
 	Int   int64
-	Valid bool // Valid is true if Bool is not NULL
+	Valid bool // Valid is true if Duration is not NULL
 }
 
 // Scan implements the Scanner interface.
@@ -441,4 +441,67 @@ func (ns *NullInt64) UnmarshalJSON(data []byte) error {
 	}
 	ns.Valid = true
 	return errors.WithStack(json.Unmarshal(data, &ns.Int))
+}
+
+// NullDuration represents a nullable JSON and SQL compatible time.Duration.
+//
+// swagger:type string
+// swagger:model nullDuration
+type NullDuration struct {
+	Duration time.Duration
+	Valid    bool
+}
+
+// Scan implements the Scanner interface.
+func (ns *NullDuration) Scan(value interface{}) error {
+	var d = sql.NullInt64{}
+	if err := d.Scan(value); err != nil {
+		return err
+	}
+
+	ns.Duration = time.Duration(d.Int64)
+	ns.Valid = d.Valid
+	return nil
+}
+
+// Value implements the driver Valuer interface.
+func (ns NullDuration) Value() (driver.Value, error) {
+	if !ns.Valid {
+		return nil, nil
+	}
+	return int64(ns.Duration), nil
+}
+
+// MarshalJSON returns m as the JSON encoding of m.
+func (ns NullDuration) MarshalJSON() ([]byte, error) {
+	if !ns.Valid {
+		return []byte("null"), nil
+	}
+
+	return json.Marshal(ns.Duration.String())
+}
+
+// UnmarshalJSON sets *m to a copy of data.
+func (ns *NullDuration) UnmarshalJSON(data []byte) error {
+	if ns == nil {
+		return errors.New("json.RawMessage: UnmarshalJSON on nil pointer")
+	}
+
+	if len(data) == 0 || string(data) == "null" {
+		return nil
+	}
+
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	p, err := time.ParseDuration(s)
+	if err != nil {
+		return err
+	}
+
+	ns.Duration = p
+	ns.Valid = true
+	return nil
 }
