4bce511110839a8e3bda79963364f0fea2d5a5bf
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-08-18 17:11:42 +0200

    feat: improved servicelocator

---
+++ servicelocatorx/options_test.go
@@ -0,0 +1,25 @@
+package servicelocatorx
+
+import (
+	"testing"
+
+	"github.com/ory/x/contextx"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOptions(t *testing.T) {
+	t.Run("case=has default contextualizer", func(t *testing.T) {
+		assert.Equal(t, &contextx.Default{}, NewOptions().Contextualizer())
+	})
+
+	t.Run("case=overwrites contextualizer", func(t *testing.T) {
+		ctxer := &struct {
+			contextx.Default
+			x string
+		}{x: "x"}
+
+		opts := NewOptions(WithContextualizer(ctxer))
+		assert.Equal(t, ctxer, opts.Contextualizer())
+	})
+}
