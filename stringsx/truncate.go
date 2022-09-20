fe0a4c28e42ac6772bf6de129491118cdf4c8753
Ajay Kelkar <ajaykelkar@outlook.com>
2022-09-20 15:39:11 +0200

    refactor: invert length check
    Co-authored-by: Patrik <zepatrik@users.noreply.github.com>

--- stringsx/truncate.go
+++ stringsx/truncate.go
@@ -4,14 +4,15 @@ import "unicode/utf8"
 
 // TruncateByteLen returns string truncated at the end with the length specified
 func TruncateByteLen(s string, length int) string {
-	if length > 0 && len(s) > length {
-		res := s[:length]
-
-		// in case we cut in the middle of an utf8 rune, we have to remove the last byte as well until it fits
-		for !utf8.ValidString(res) {
-			res = res[:len(res)-1]
-		}
-		return res
+	if length <= 0 || len(s) <= length {
+		return s
 	}
-	return s
+
+	res := s[:length]
+
+	// in case we cut in the middle of an utf8 rune, we have to remove the last byte as well until it fits
+	for !utf8.ValidString(res) {
+		res = res[:len(res)-1]
+	}
+	return res
 }
