83f5bce1189c2d294b181a59eb5e53f9a46791d0
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-10-05 13:53:01 +0200

    feat: export pagination headers

--- pagination/tokenpagination/pagination.go
+++ pagination/tokenpagination/pagination.go
@@ -27,6 +27,30 @@ func decode(s string) (int, error) {
 	return int(gjson.Get(string(b), "page").Int()), nil
 }
 
+// swagger:model headerTokenPagination
+type HeaderAnnotation struct {
+	// The Link HTTP Header
+	//
+	// The `Link` header contains a comma-delimited list of links to the following pages:
+	//
+	// - first: The first page of results.
+	// - next: The next page of results.
+	// - prev: The previous page of results.
+	// - last: The last page of results.
+	//
+	// Pages are omitted if they do not exist. For example, if there is no next page, the `next` link is omitted.
+	//
+	//	Example: Link: </clients?limit=5&offset=0>; rel="first",</clients?limit=5&offset=15>; rel="next",</clients?limit=5&offset=5>; rel="prev",</clients?limit=5&offset=20>; rel="last"
+	Link string `json:"link"`
+
+	// The X-Total-Count HTTP Header
+	//
+	// The `X-Total-Count` header contains the total number of items in the collection.
+	//
+	// Example: 123
+	TotalCount int `json:"x-total-count"`
+}
+
 type TokenPaginator struct {
 	MaxItems     int
 	DefaultItems int
