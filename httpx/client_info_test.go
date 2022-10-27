3e45d42ce77373066f19228b8203428ab21674c2
hackerman <3372410+aeneasr@users.noreply.github.com>
2022-10-27 08:44:14 +0200

    fix: simplify ip validator (#624)
    Closes https://github.com/ory-corp/cloud/issues/3201
    
    (cherry picked from commit 585b473819272a7c4aeae4cab265fce232743f5a)

--- httpx/client_info_test.go
+++ httpx/client_info_test.go
@@ -11,13 +11,13 @@ import (
 func TestIgnoresInternalIPs(t *testing.T) {
 	input := "54.155.246.232,10.145.1.10"
 
-	res, err := GetClientIPAddress(strings.Split(input, ","), InternalIPSet)
+	res, err := GetClientIPAddressesWithoutInternalIPs(strings.Split(input, ","))
 	require.NoError(t, err)
 	assert.Equal(t, "54.155.246.232", res)
 }
 
 func TestEmptyInputArray(t *testing.T) {
-	res, err := GetClientIPAddress([]string{}, InternalIPSet)
+	res, err := GetClientIPAddressesWithoutInternalIPs([]string{})
 	require.NoError(t, err)
 	assert.Equal(t, "", res)
 }
