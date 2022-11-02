9eb8b953307cb757cbdb43faffc085fc775c1d9f
Arne Luenser <arne.luenser@ory.sh>
2022-11-02 14:59:08 +0100

    feat: add package otelx/semconv
    Contains OpenTelemetry semantic convention constants.

---
+++ otelx/semconv/events.go
@@ -0,0 +1,12 @@
+// Package semconv contains OpenTelemetry semantic convention constants.
+package semconv
+
+const (
+	EventSessionIssued   = "Session issued"
+	EventIdentityCreated = "Identity created"
+)
+
+const (
+	AttrIdentityID = "identity.id"
+	AttrNID        = "nid"
+)
