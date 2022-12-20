3b6a608bf5ef960752d4253b2eb469c5e55bdfbe
Arne <arne.luenser@ory.sh>
2022-12-20 13:49:52 +0100

    chore: cleanup licence headers (#647)

--- healthx/handler.go
+++ healthx/handler.go
@@ -1,26 +1,6 @@
 // Copyright © 2022 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-/*
- * Copyright © 2015-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * @author		Aeneas Rekkas <aeneas+oss@aeneas.io>
- * @copyright 	2015-2018 Aeneas Rekkas <aeneas+oss@aeneas.io>
- * @license 	Apache-2.0
- */
-
 package healthx
 
 import (
@@ -32,7 +12,7 @@ import (
 const (
 	// AliveCheckPath is the path where information about the life state of the instance is provided.
 	AliveCheckPath = "/health/alive"
-	// ReadyCheckPath is the path where information about the rady state of the instance is provided.
+	// ReadyCheckPath is the path where information about the ready state of the instance is provided.
 	ReadyCheckPath = "/health/ready"
 	// VersionPath is the path where information about the software version of the instance is provided.
 	VersionPath = "/version"
