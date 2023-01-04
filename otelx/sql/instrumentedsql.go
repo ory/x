805605aff3b1783cd0898faf7df2d8010aae2daf
Arne <arne.luenser@ory.sh>
2023-01-04 13:37:44 +0100

    fix: clean up instrumentedsql (#652)

--- otelx/sql/instrumentedsql.go
+++ otelx/sql/instrumentedsql.go
@@ -8,7 +8,6 @@ import (
 	"database/sql/driver"
 
 	"github.com/luna-duclos/instrumentedsql"
-	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
@@ -16,41 +15,32 @@ import (
 
 const tracingComponent = "github.com/ory/x/otelx/sql"
 
-type tracer struct{}
+type (
+	tracer struct{}
+	span   struct {
+		ctx    context.Context
+		parent trace.Span
+	}
+)
 
-type span struct {
-	tracer
-	ctx    context.Context
-	parent trace.Span
-}
+var (
+	_ instrumentedsql.Tracer = tracer{}
+	_ instrumentedsql.Span   = span{}
+)
 
 func NewTracer() instrumentedsql.Tracer { return tracer{} }
 
 // GetSpan returns a span
-func (t tracer) GetSpan(ctx context.Context) instrumentedsql.Span {
-	if ctx == nil {
-		return span{ctx: nil}
-	}
-
-	return span{parent: trace.SpanFromContext(ctx), tracer: t}
+func (tracer) GetSpan(ctx context.Context) instrumentedsql.Span {
+	return span{ctx, trace.SpanFromContext(ctx)}
 }
 
 func (s span) NewChild(name string) instrumentedsql.Span {
-	if s.ctx == nil {
-		return s
-	}
-
-	var parent trace.Span
-	tp := otel.GetTracerProvider().Tracer(tracingComponent)
-	_, parent = tp.Start(s.ctx, name)
-
-	return span{ctx: s.ctx, parent: parent}
+	ctx, child := s.parent.TracerProvider().Tracer(tracingComponent).Start(s.ctx, name, trace.WithSpanKind(trace.SpanKindClient))
+	return span{ctx, child}
 }
 
 func (s span) SetLabel(k, v string) {
-	if s.parent == nil {
-		return
-	}
 	s.parent.SetAttributes(attribute.String(k, v))
 }
 
@@ -58,20 +48,9 @@ func (s span) SetError(err error) {
 	if err == nil || err == driver.ErrSkip {
 		return
 	}
-
-	if s.parent == nil {
-		return
-	}
-
 	s.parent.SetStatus(codes.Error, err.Error())
-	s.parent.AddEvent("error", trace.WithAttributes(
-		attribute.String("message", err.Error())),
-	)
 }
 
 func (s span) Finish() {
-	if s.parent == nil {
-		return
-	}
 	s.parent.End()
 }
