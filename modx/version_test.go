685f4fc75b5b8d15fda336e331c55b188f4df7aa
Misa Munde <misa.munde@gmail.com>
2023-05-31 12:48:00 +0300

    feat: sqa metrics v2

--- modx/version_test.go
+++ modx/version_test.go
@@ -51,7 +51,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/openzipkin-contrib/zipkin-go-opentracing v0.4.5
 	github.com/openzipkin/zipkin-go v0.2.2
-	github.com/ory/analytics-go/v4 v4.0.0
+	github.com/ory/analytics-go/v5 v5.0.0
 	github.com/ory/dockertest/v3 v3.6.3
 	github.com/ory/go-acc v0.2.6
 	github.com/ory/herodot v0.9.2
