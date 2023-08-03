19829a931daa7e60675daa8d53efedb13051959f
Arne Luenser <arne.luenser@ory.sh>
2023-08-03 10:05:11 +0200

    feat: add more tests to fsx

--- fsx/merge_test.go
+++ fsx/merge_test.go
@@ -11,20 +11,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMergeFS(t *testing.T) {
-	a := fstest.MapFS{
+var (
+	a = fstest.MapFS{
 		"a":     &fstest.MapFile{},
 		"dir/c": &fstest.MapFile{},
 	}
-	b := fstest.MapFS{
+	b = fstest.MapFS{
 		"b":     &fstest.MapFile{},
 		"dir/d": &fstest.MapFile{},
 	}
-	x := fstest.MapFS{
+	x = fstest.MapFS{
 		"x":     &fstest.MapFile{},
 		"dir/y": &fstest.MapFile{},
 	}
+)
 
+func TestMergeFS(t *testing.T) {
 	assert.NoError(t, fstest.TestFS(
 		Merge(a, b),
 		"a",
@@ -44,7 +46,39 @@ func TestMergeFS(t *testing.T) {
 		"dir/y",
 		"x",
 	))
+	assert.NoError(t, fstest.TestFS(
+		Merge(x, b, a),
+		"a",
+		"b",
+		"dir",
+		"dir/c",
+		"dir/d",
+		"dir/y",
+		"x",
+	))
+	assert.NoError(t, fstest.TestFS(
+		Merge(Merge(a, b), x),
+		"a",
+		"b",
+		"dir",
+		"dir/c",
+		"dir/d",
+		"dir/y",
+		"x",
+	))
+	assert.NoError(t, fstest.TestFS(
+		Merge(Merge(x, b), a),
+		"a",
+		"b",
+		"dir",
+		"dir/c",
+		"dir/d",
+		"dir/y",
+		"x",
+	))
+}
 
+func TestLaherMergeFS(t *testing.T) {
 	assert.Error(t, fstest.TestFS(
 		mergefs.Merge(a, b),
 		"a",
@@ -54,8 +88,30 @@ func TestMergeFS(t *testing.T) {
 		"dir/d",
 	))
 
-	assert.Error(t, fstest.TestFS(
+	t.Skip("laher/mergefs does not handle recursive merges correctly")
+
+	assert.NoError(t, fstest.TestFS(
 		mergefs.Merge(mergefs.Merge(a, b), x),
+		"a",
+		"b",
+		"dir",
+		"dir/c",
+		"dir/d",
+		"dir/y",
+		"x",
+	))
+	assert.NoError(t, fstest.TestFS(
+		mergefs.Merge(a, mergefs.Merge(b, x)),
+		"a",
+		"b",
+		"dir",
+		"dir/c",
+		"dir/d",
+		"dir/y",
+		"x",
+	))
+	assert.NoError(t, fstest.TestFS(
+		mergefs.Merge(x, mergefs.Merge(b, a)),
 		"a",
 		"b",
 		"dir",
