e4313246e20f37103c1623830735e5101bfa1ee2
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2023-08-07 11:00:17 +0200

    chore: add retry testhelper

---
+++ retry/retry.go
@@ -0,0 +1,281 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// From https://github.com/hashicorp/consul/blob/sdk/v0.14.1/sdk/testutil/retry/retry.go
+
+// Package retry provides support for repeating operations in tests.
+//
+// A sample retry operation looks like this:
+//
+//	func TestX(t *testing.T) {
+//	    retry.Run(t, func(r *retry.R) {
+//	        if err := foo(); err != nil {
+//				r.Errorf("foo: %s", err)
+//				return
+//	        }
+//	    })
+//	}
+//
+// Run uses the DefaultFailer, which is a Timer with a Timeout of 7s,
+// and a Wait of 25ms. To customize, use RunWith.
+//
+// WARNING: unlike *testing.T, *retry.R#Fatal and FailNow *do not*
+// fail the test function entirely, only the current run the retry func
+package retry
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+	"strings"
+	"time"
+)
+
+// Failer is an interface compatible with testing.T.
+type Failer interface {
+	Helper()
+
+	// Log is called for the final test output
+	Log(args ...interface{})
+
+	// FailNow is called when the retrying is abandoned.
+	FailNow()
+}
+
+// R provides context for the retryer.
+//
+// Logs from Logf, (Error|Fatal)(f) are gathered in an internal buffer
+// and printed only if the retryer fails. Printed logs are deduped and
+// prefixed with source code line numbers
+type R struct {
+	// fail is set by FailNow and (Fatal|Error)(f). It indicates the pass
+	// did not succeed, and should be retried
+	fail bool
+	// done is set by Stop. It indicates the entire run was a failure,
+	// and triggers t.FailNow()
+	done   bool
+	output []string
+}
+
+func (r *R) Logf(format string, args ...interface{}) {
+	r.log(fmt.Sprintf(format, args...))
+}
+
+func (r *R) Log(args ...interface{}) {
+	r.log(fmt.Sprintln(args...))
+}
+
+func (r *R) Helper() {}
+
+// runFailed is a sentinel value to indicate that the func itself
+// didn't panic, rather that `FailNow` was called.
+type runFailed struct{}
+
+// FailNow stops run execution. It is roughly equivalent to:
+//
+//	r.Error("")
+//	return
+//
+// inside the function being run.
+func (r *R) FailNow() {
+	r.fail = true
+	panic(runFailed{})
+}
+
+// Fatal is equivalent to r.Logf(args) followed by r.FailNow(), i.e. the run
+// function should be exited. Retries on the next run are allowed. Fatal is
+// equivalent to
+//
+//	r.Error(args)
+//	return
+//
+// inside the function being run.
+func (r *R) Fatal(args ...interface{}) {
+	r.log(fmt.Sprint(args...))
+	r.FailNow()
+}
+
+// Fatalf is like Fatal but allows a format string
+func (r *R) Fatalf(format string, args ...interface{}) {
+	r.log(fmt.Sprintf(format, args...))
+	r.FailNow()
+}
+
+// Error indicates the current run encountered an error and should be retried.
+// It *does not* stop execution of the rest of the run function.
+func (r *R) Error(args ...interface{}) {
+	r.log(fmt.Sprint(args...))
+	r.fail = true
+}
+
+// Errorf is like Error but allows a format string
+func (r *R) Errorf(format string, args ...interface{}) {
+	r.log(fmt.Sprintf(format, args...))
+	r.fail = true
+}
+
+// If err is non-nil, equivalent to r.Fatal(err.Error()) followed by
+// r.FailNow(). Otherwise a no-op.
+func (r *R) Check(err error) {
+	if err != nil {
+		r.log(err.Error())
+		r.FailNow()
+	}
+}
+
+func (r *R) log(s string) {
+	r.output = append(r.output, decorate(s))
+}
+
+// Stop retrying, and fail the test, logging the specified error.
+// Does not stop execution, so return should be called after.
+func (r *R) Stop(err error) {
+	r.log(err.Error())
+	r.done = true
+}
+
+func decorate(s string) string {
+	_, file, line, ok := runtime.Caller(3)
+	if ok {
+		n := strings.LastIndex(file, "/")
+		if n >= 0 {
+			file = file[n+1:]
+		}
+	} else {
+		file = "???"
+		line = 1
+	}
+	return fmt.Sprintf("%s:%d: %s", file, line, s)
+}
+
+func Run(t Failer, f func(r *R)) {
+	t.Helper()
+	run(DefaultFailer(), t, f)
+}
+
+func RunWith(r Retryer, t Failer, f func(r *R)) {
+	t.Helper()
+	run(r, t, f)
+}
+
+func dedup(a []string) string {
+	if len(a) == 0 {
+		return ""
+	}
+	seen := map[string]struct{}{}
+	var b bytes.Buffer
+	for _, s := range a {
+		if _, ok := seen[s]; ok {
+			continue
+		}
+		seen[s] = struct{}{}
+		b.WriteString(s)
+		b.WriteRune('\n')
+	}
+	return b.String()
+}
+
+func run(r Retryer, t Failer, f func(r *R)) {
+	t.Helper()
+	rr := &R{}
+
+	fail := func() {
+		t.Helper()
+		out := dedup(rr.output)
+		if out != "" {
+			t.Log(out)
+		}
+		t.FailNow()
+	}
+
+	for r.Continue() {
+		// run f(rr), but if recover yields a runFailed value, we know
+		// FailNow was called.
+		func() {
+			defer func() {
+				if p := recover(); p != nil && p != (runFailed{}) {
+					panic(p)
+				}
+			}()
+			f(rr)
+		}()
+
+		switch {
+		case rr.done:
+			fail()
+			return
+		case !rr.fail:
+			return
+		}
+		rr.fail = false
+	}
+	fail()
+}
+
+// DefaultFailer provides default retry.Run() behavior for unit tests, namely
+// 7s timeout with a wait of 25ms
+func DefaultFailer() *Timer {
+	return &Timer{Timeout: 7 * time.Second, Wait: 25 * time.Millisecond}
+}
+
+// TwoSeconds repeats an operation for two seconds and waits 25ms in between.
+func TwoSeconds() *Timer {
+	return &Timer{Timeout: 2 * time.Second, Wait: 25 * time.Millisecond}
+}
+
+// ThreeTimes repeats an operation three times and waits 25ms in between.
+func ThreeTimes() *Counter {
+	return &Counter{Count: 3, Wait: 25 * time.Millisecond}
+}
+
+// Retryer provides an interface for repeating operations
+// until they succeed or an exit condition is met.
+type Retryer interface {
+	// Continue returns true if the operation should be repeated, otherwise it
+	// returns false to indicate retrying should stop.
+	Continue() bool
+}
+
+// Counter repeats an operation a given number of
+// times and waits between subsequent operations.
+type Counter struct {
+	Count int
+	Wait  time.Duration
+
+	count int
+}
+
+func (r *Counter) Continue() bool {
+	if r.count == r.Count {
+		return false
+	}
+	if r.count > 0 {
+		time.Sleep(r.Wait)
+	}
+	r.count++
+	return true
+}
+
+// Timer repeats an operation for a given amount
+// of time and waits between subsequent operations.
+type Timer struct {
+	Timeout time.Duration
+	Wait    time.Duration
+
+	// stop is the timeout deadline.
+	// TODO: Next()?
+	// Set on the first invocation of Next().
+	stop time.Time
+}
+
+func (r *Timer) Continue() bool {
+	if r.stop.IsZero() {
+		r.stop = time.Now().Add(r.Timeout)
+		return true
+	}
+	if time.Now().After(r.stop) {
+		return false
+	}
+	time.Sleep(r.Wait)
+	return true
+}
---
+++ retry/retry_test.go
@@ -0,0 +1,148 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+// From https://github.com/hashicorp/consul/blob/sdk/v0.14.1/sdk/testutil/retry/retry_test.go
+
+package retry
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// delta defines the time band a test run should complete in.
+var delta = 25 * time.Millisecond
+
+func TestRetryer(t *testing.T) {
+	tests := []struct {
+		desc string
+		r    Retryer
+	}{
+		{"counter", &Counter{Count: 3, Wait: 100 * time.Millisecond}},
+		{"timer", &Timer{Timeout: 200 * time.Millisecond, Wait: 100 * time.Millisecond}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			var iters int
+			start := time.Now()
+			for tt.r.Continue() {
+				iters++
+			}
+			dur := time.Since(start)
+			if got, want := iters, 3; got != want {
+				t.Fatalf("got %d retries want %d", got, want)
+			}
+			// since the first iteration happens immediately
+			// the retryer waits only twice for three iterations.
+			// order of events: (true, (wait) true, (wait) true, false)
+			if got, want := dur, 200*time.Millisecond; got < (want-delta) || got > (want+delta) {
+				t.Fatalf("loop took %v want %v (+/- %v)", got, want, delta)
+			}
+		})
+	}
+}
+
+func TestBasics(t *testing.T) {
+	t.Run("Error allows retry", func(t *testing.T) {
+		i := 0
+		Run(t, func(r *R) {
+			i++
+			t.Logf("i: %d; r: %#v", i, r)
+			if i == 1 {
+				r.Errorf("Errorf, i: %d", i)
+				return
+			}
+		})
+		assert.Equal(t, i, 2)
+	})
+
+	t.Run("Fatal returns from func, but does not fail test", func(t *testing.T) {
+		i := 0
+		gotHere := false
+		ft := &fakeT{}
+		Run(ft, func(r *R) {
+			i++
+			t.Logf("i: %d; r: %#v", i, r)
+			if i == 1 {
+				r.Fatalf("Fatalf, i: %d", i)
+				gotHere = true
+			}
+		})
+
+		assert.False(t, gotHere)
+		assert.Equal(t, i, 2)
+		// surprisingly, r.FailNow() *does not* trigger ft.FailNow()!
+		assert.Equal(t, ft.fails, 0)
+	})
+
+	t.Run("Func being run can panic with struct{}{}", func(t *testing.T) {
+		gotPanic := false
+		func() {
+			defer func() {
+				if p := recover(); p != nil {
+					gotPanic = true
+				}
+			}()
+			Run(t, func(r *R) {
+				panic(struct{}{})
+			})
+		}()
+
+		assert.True(t, gotPanic)
+	})
+}
+
+func TestRunWith(t *testing.T) {
+	t.Run("calls FailNow after exceeding retries", func(t *testing.T) {
+		ft := &fakeT{}
+		iter := 0
+		RunWith(&Counter{Count: 3, Wait: time.Millisecond}, ft, func(r *R) {
+			iter++
+			r.FailNow()
+		})
+
+		require.Equal(t, 3, iter)
+		require.Equal(t, 1, ft.fails)
+	})
+
+	t.Run("Stop ends the retrying", func(t *testing.T) {
+		ft := &fakeT{}
+		iter := 0
+		RunWith(&Counter{Count: 5, Wait: time.Millisecond}, ft, func(r *R) {
+			iter++
+			if iter == 2 {
+				r.Stop(fmt.Errorf("do not proceed"))
+			}
+			r.Fatalf("not yet")
+		})
+
+		// TODO: these should all be assert
+		require.Equal(t, 2, iter)
+		require.Equal(t, 1, ft.fails)
+		require.Len(t, ft.out, 1)
+		require.Contains(t, ft.out[0], "not yet\n")
+		require.Contains(t, ft.out[0], "do not proceed\n")
+	})
+}
+
+type fakeT struct {
+	fails int
+	out   []string
+}
+
+func (f *fakeT) Helper() {}
+
+func (f *fakeT) Log(args ...interface{}) {
+	f.out = append(f.out, fmt.Sprint(args...))
+}
+
+func (f *fakeT) FailNow() {
+	f.fails++
+}
+
+var _ Failer = &fakeT{}
