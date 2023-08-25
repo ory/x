577e4fb62975a05ca9b973b2004b4b43d77573e9
hackerman <3372410+aeneasr@users.noreply.github.com>
2023-08-25 08:59:06 +0200

    feat: add DirtyPatch method (#719)
    It's fast:
    
    ```
    BenchmarkSet
    BenchmarkSet-8                       	    6189	   9197403 ns/op
    BenchmarkDirtyPatch
    BenchmarkDirtyPatch-8                	  401864	      3057 ns/op
    ```

--- configx/provider_test.go
+++ configx/provider_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestProviderMethods(t *testing.T) {
+func newProvider(t testing.TB) *Provider {
 	// Fake some flags
 	f := pflag.NewFlagSet("config", pflag.ContinueOnError)
 	f.String("foo-bar-baz", "", "")
@@ -32,10 +32,15 @@ func TestProviderMethods(t *testing.T) {
 	RegisterFlags(f)
 
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	t.Cleanup(cancel)
 
 	p, err := New(ctx, []byte(`{"type": "object", "properties": {"foo-bar-baz": {"type": "string"}, "b": {"type": "string"}}}`), WithFlags(f), WithContext(ctx))
 	require.NoError(t, err)
+	return p
+}
+
+func TestProviderMethods(t *testing.T) {
+	p := newProvider(t)
 
 	t.Run("check flags", func(t *testing.T) {
 		assert.Equal(t, "fff", p.String("foo-bar-baz"))
@@ -105,6 +110,21 @@ func TestProviderMethods(t *testing.T) {
 		assert.NoError(t, p.Set("nested", nil))
 		assert.NoError(t, p.Set("nested.value", "https://www.ory.sh/kratos"))
 		assert.Equal(t, "https://www.ory.sh/kratos", p.Get("nested.value"))
+	})
+
+	t.Run("use DirtyPatch operations", func(t *testing.T) {
+		assert.NoError(t, p.DirtyPatch("nested", nil))
+		assert.NoError(t, p.DirtyPatch("nested.value", "https://www.ory.sh/kratos"))
+		assert.Equal(t, "https://www.ory.sh/kratos", p.Get("nested.value"))
+
+		assert.NoError(t, p.DirtyPatch("duration.integer1", -1))
+		assert.NoError(t, p.DirtyPatch("duration.integer2", "-1"))
+		assert.Equal(t, -1*time.Nanosecond, p.DurationF("duration.integer1", time.Second))
+		assert.Equal(t, -1*time.Nanosecond, p.DurationF("duration.integer2", time.Second))
+
+		require.NoError(t, p.DirtyPatch("some.float", 123.123))
+		assert.Equal(t, 123.123, p.Float64F("some.float", 321.321))
+		assert.Equal(t, 321.321, p.Float64F("not.some.float", 321.321))
 	})
 }
 
@@ -210,5 +230,29 @@ func TestAdvancedConfigs(t *testing.T) {
 				tc.expectedF(t, k)
 			}
 		})
+	}
+}
+
+func BenchmarkSet(b *testing.B) {
+	// Benchmark set function
+	p := newProvider(b)
+	var err error
+	for i := 0; i < b.N; i++ {
+		err = p.Set("nested.value", "https://www.ory.sh/kratos")
+		if err != nil {
+			b.Fatalf("Unexpected error: %s", err)
+		}
+	}
+}
+
+func BenchmarkDirtyPatch(b *testing.B) {
+	// Benchmark set function
+	p := newProvider(b)
+	var err error
+	for i := 0; i < b.N; i++ {
+		err = p.DirtyPatch("nested.value", "https://www.ory.sh/kratos")
+		if err != nil {
+			b.Fatalf("Unexpected error: %s", err)
+		}
 	}
 }
