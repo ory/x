53c3843f806255ced18bded0d7cc8808e74291d3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-10-11 12:46:59 +0200

    docs: update docstring for consistency

--- crdbx/staleness.go
+++ crdbx/staleness.go
@@ -17,17 +17,22 @@ import (
 //
 // swagger:model consistencyRequestParameters
 type ConsistencyRequestParameters struct {
-	// Read Consistency Level (experimental)
+	// Read Consistency Level (preview)
 	//
-	// The read consistency level determines the consistency guarantee for reads and queries:
+	// The read consistency level determines the consistency guarantee for reads:
 	//
 	// - strong (slow): The read is guaranteed to return the most recent data committed at the start of the read.
 	// - eventual (very fast): The result will return data that is about 4.8 seconds old.
 	//
 	// The default consistency guarantee can be changed in the Ory Network Console or using the Ory CLI with
-	// `ory patch project --replace '/serve/default_consistency_level="strong"'`.
+	// `ory patch project --replace '/previews/default_read_consistency_level="strong"'`.
 	//
-	// This feature is fully functional only in Ory Network and currently experimental.
+	// Setting the default consistency level to `eventual` may cause regressions in the future as we add consistency
+	// controls to more APIs. Currently, the following APIs will be affected by this setting:
+	//
+	// - `GET /admin/identities`
+	//
+	// This feature is in preview and only available in Ory Network.
 	//
 	// required: false
 	// in: query
