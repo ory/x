4f89db5126ce6d3d0648a0df6aa5128b9f1b81c5
Philippe Gaultier <philippe.gaultier@ory.sh>
2025-05-13 11:26:01 +0200

    comment

--- jsonnetsecure/jsonnet_processvm.go
+++ jsonnetsecure/jsonnet_processvm.go
@@ -34,6 +34,9 @@ func NewProcessVM(opts *vmOptions) VM {
 	}
 }
 
+// Jsonnet evaluation is run in a subprocess with a timeout.
+// Standard output and error are limited in size and are truncated to this limit
+// if too big (but this is not an error condition).
 func (p *ProcessVM) EvaluateAnonymousSnippet(filename string, snippet string) (_ string, err error) {
 	tracer := trace.SpanFromContext(p.ctx).TracerProvider().Tracer("")
 	ctx, span := tracer.Start(p.ctx, "jsonnetsecure.ProcessVM.EvaluateAnonymousSnippet", trace.WithAttributes(attribute.String("filename", filename)))
