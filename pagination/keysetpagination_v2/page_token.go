2d19583398c701288e1479e5e413e3965b18c02a
zepatrik <zepatrik@users.noreply.github.com>
2025-06-18 15:26:31 +0200

    feat(pagination): improved keyset pagination with encrypted tokens

---
+++ pagination/keysetpagination_v2/page_token.go
@@ -0,0 +1,67 @@
+// Copyright © 2023 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+package keysetpagination
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/ssoready/hyrumtoken"
+
+	"github.com/ory/herodot"
+)
+
+type (
+	PageToken struct {
+		testNow func() time.Time
+		cols    []Column
+	}
+	jsonPageToken = struct {
+		ExpiresAt time.Time `json:"e"`
+		Cols      []Column  `json:"c"`
+	}
+	Column struct {
+		Name  string `json:"n"`
+		Order Order  `json:"o"`
+		Value any    `json:"v"`
+	}
+)
+
+func (t PageToken) Columns() []Column            { return t.cols }
+func (t PageToken) Encrypt(key *[32]byte) string { return hyrumtoken.Marshal(key, t) }
+
+func (t PageToken) MarshalJSON() ([]byte, error) {
+	now := time.Now
+	if t.testNow != nil {
+		now = t.testNow
+	}
+	toEncode := jsonPageToken{
+		ExpiresAt: now().Add(time.Hour).UTC(),
+		Cols:      t.cols,
+	}
+	return json.Marshal(toEncode)
+}
+
+var ErrPageTokenExpired = herodot.ErrBadRequest.WithReason("page token expired, do not persist page tokens")
+
+func (t *PageToken) UnmarshalJSON(data []byte) error {
+	rawToken := jsonPageToken{}
+	if err := json.Unmarshal(data, &rawToken); err != nil {
+		return err
+	}
+	t.cols = rawToken.Cols
+	now := time.Now
+	if t.testNow != nil {
+		now = t.testNow
+	}
+	if rawToken.ExpiresAt.Before(now().UTC()) {
+		return errors.WithStack(ErrPageTokenExpired)
+	}
+	return nil
+}
+
+func NewPageToken(cols ...Column) PageToken {
+	return PageToken{cols: cols}
+}
